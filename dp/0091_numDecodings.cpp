ede4c014b4d8862018047256b262092308bbc154
zephyr-bj <zephyrutd@gmail.com>
2019-04-28 13:02:29 -0700

    Create 0091_numDecodings.cpp

---
+++ dp/0091_numDecodings.cpp
@@ -0,0 +1,21 @@
+    int numDecodings(string s) {
+        int n = s.size();
+        if(n==0)return 0;
+        if(s[0]=='0')return 0;
+        vector<int>dp(n+1,0);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=1; i<n; i++){
+            if(s[i]=='0'){
+                if(s[i-1]=='1'||s[i-1]=='2')dp[i+1]=dp[i-1];
+                else return 0;
+            }else if(s[i]>'0'&&s[i]<'7'){
+                if(s[i-1]=='1'||s[i-1]=='2')dp[i+1]=dp[i]+dp[i-1];
+                else dp[i+1]=dp[i];
+            }else{
+                if(s[i-1]=='1')dp[i+1]=dp[i]+dp[i-1];
+                else dp[i+1]=dp[i];
+            }
+        }
+        return dp[n];
+    }
