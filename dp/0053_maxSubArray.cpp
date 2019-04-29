d24ab634666bdea2c52dd3a4de9e2ec425739dec
zephyr-bj <zephyrutd@gmail.com>
2019-04-28 22:25:20 -0700

    Create 0053_maxSubArray.cpp

---
+++ dp/0053_maxSubArray.cpp
@@ -0,0 +1,8 @@
+    int maxSubArray(vector<int>& nums) {
+        int ans=nums[0];
+        for(int i=1; i<nums.size(); i++){
+            nums[i]=max(nums[i],nums[i]+nums[i-1]);
+            ans=max(ans,nums[i]);
+        }
+        return ans;
+    }
