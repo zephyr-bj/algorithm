f7983b732a090f5c9094aefbee6c374b378d0c64
zephyr-bj <zephyrutd@gmail.com>
2019-04-28 21:57:59 -0700

    Create 0064_minPathSum.cpp

---
+++ dp/0064_minPathSum.cpp
@@ -0,0 +1,17 @@
+    int minPathSum(vector<vector<int>>& grid) {
+        int m=grid.size();
+        if(m==0)return 0;
+        int n=grid[0].size();
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i-1>=0&&j-1>=0){
+                    grid[i][j]+=min(grid[i][j-1],grid[i-1][j]);
+                }else if(i-1>=0&&j-1<0){
+                    grid[i][j]+=grid[i-1][j];
+                }else if(i-1<0&&j-1>=0){
+                    grid[i][j]+=grid[i][j-1];
+                }
+            }
+        }
+        return grid[m-1][n-1];
+    }
