f189f0c662bd5340128465ab2d72a1d178baa8bf
zephyr-bj <zephyrutd@gmail.com>
2019-05-01 09:18:34 -0700

    Create 0102_levelOrder.cpp

---
+++ tree/0102_levelOrder.cpp
@@ -0,0 +1,36 @@
+    void tool(vector<vector<int>>&bin, TreeNode * root, int level){
+        if(root==NULL)return;
+        tool(bin,root->left,level+1);
+        while(bin.size()<level+1){
+            vector<int>tmp(0,0);
+            bin.push_back(tmp);
+        }
+        bin[level].push_back(root->val);
+        tool(bin,root->right,level+1);
+    }
+
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>>ans;
+        if(root==NULL)return ans;
+        tool(ans,root,0);
+        return ans;
+    }
+    /*
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>>ans;
+        if(root==NULL)return ans;
+        queue<TreeNode*>bin;
+        bin.push(root);
+        while(!bin.empty()){
+            vector<int>level;
+            int L = bin.size();
+            for(int i=0; i<L; i++){
+                level.push_back(bin.front()->val);
+                if(bin.front()->left!=NULL)bin.push(bin.front()->left);
+                if(bin.front()->right!=NULL)bin.push(bin.front()->right);
+                bin.pop();
+            }
+            ans.push_back(level);
+        }
+        return ans;
+    }*/
