be4bc55e6cd64fd98aaa8e8609111b46f7bfdb2f
zephyr-bj <zephyrutd@gmail.com>
2019-05-02 17:41:21 -0700

    Update 0133_cloneGraph.cpp

--- directed_graph/0133_cloneGraph.cpp
+++ directed_graph/0133_cloneGraph.cpp
@@ -1,9 +1,11 @@
-    unordered_map<int, Node* > m;
+    unordered_map<int, Node* > visited;
     Node* cloneGraph(Node* node) {
         if (!node) return NULL;
-        auto clone = new Node(node->val);
-        m[node->val] = clone;
-        for (auto nb : node->neighbors) 
-            clone->neighbors.push_back(m.count(nb->val) ? m[nb->val] : cloneGraph(nb) );
+        Node * clone = new Node(node->val);
+        visited[node->val] = clone;
+        for (vector<Node*>::iterator it=node->neighbors.begin(); it!=node->neighbors.end(); it++){
+            if(visited.find((*it)->val)!=visited.end())clone->neighbors.push_back(visited[(*it)->val]);
+            else clone->neighbors.push_back(cloneGraph(*it));
+        }
         return clone;
     }
