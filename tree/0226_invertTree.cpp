bf16a42c0195c6bc0acfe607dfad69b64fc82f61
zephyr-bj <zephyrutd@gmail.com>
2019-05-28 12:55:02 -0700

    Create 0226_invertTree.cpp

---
+++ tree/0226_invertTree.cpp
@@ -0,0 +1,9 @@
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL)return root;
+        TreeNode *tmp = root->left;
+        root->left=root->right;
+        root->right=tmp;
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
