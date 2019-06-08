36b89eba7c106a5998c913d175608fbcb5301830
zephyr-bj <zephyrutd@gmail.com>
2019-06-08 10:23:33 -0700

    Create 0300_longestIncreasingSubsequence.cpp

---
+++ ordered_set/0300_longestIncreasingSubsequence.cpp
@@ -0,0 +1,10 @@
+    int lengthOfLIS(vector<int>& nums) {
+        set<int>bin;
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            set<int>::iterator it = bin.lower_bound(nums[i]);
+            if(it!=bin.end())bin.erase(it);
+            bin.insert(nums[i]);
+        }
+        return bin.size();
+    }
