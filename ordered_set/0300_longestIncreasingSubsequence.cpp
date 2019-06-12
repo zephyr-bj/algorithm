cd7429fdc73ac990a6b6a3542fcad9073b0108f4
zephyr-bj <zephyrutd@gmail.com>
2019-06-12 12:55:53 -0700

    Update 0300_longestIncreasingSubsequence.cpp

--- ordered_set/0300_longestIncreasingSubsequence.cpp
+++ ordered_set/0300_longestIncreasingSubsequence.cpp
@@ -9,5 +9,23 @@
         return bin.size();
     }
 /*
-can not use stack rising sequence trick
+https://www.geeksforgeeks.org/longest-monotonically-increasing-subsequence-size-n-log-n/
+*/
+/*
+//a dp solution
+   int lengthOfLIS(vector<int>& nums) {
+        const int size = nums.size();
+        if (size == 0) { return 0; } 
+        vector<int> dp(size, 1);
+        int res = 1;
+        for (int i = 1; i < size; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = max(dp[i], dp[j]+1);
+                }
+            }
+            res = max (res, dp[i]);
+        }
+        return res;
+    }
 */
