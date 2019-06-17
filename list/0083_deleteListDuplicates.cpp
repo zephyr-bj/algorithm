232e49f267fea77a4b2e72d9b7c3abdd540b2caf
zephyr-bj <zephyrutd@gmail.com>
2019-06-16 22:35:28 -0700

    Update 0083_deleteListDuplicates.cpp

--- list/0083_deleteListDuplicates.cpp
+++ list/0083_deleteListDuplicates.cpp
@@ -2,7 +2,9 @@
         ListNode * q=head;
         while(q!=NULL&&q->next!=NULL){
             if(q->val==q->next->val){
+                ListNode * tmp=q->next;
                 q->next=q->next->next;
+                delete tmp;
             }else{
                 q=q->next;
             }
