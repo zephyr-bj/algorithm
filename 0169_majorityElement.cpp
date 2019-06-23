cd9713de61e4d11a291a365685f30e920c945d27
zephyr-bj <zephyrutd@gmail.com>
2019-06-22 18:43:04 -0700

    Create 0169_majorityElement.cpp

---
+++ 0169_majorityElement.cpp
@@ -0,0 +1,13 @@
+    //moore voting
+    int majorityElement(int[] num) {
+        int major=num[0], count = 1;
+        for(int i=1; i<num.length;i++){
+            if(count==0){
+                count++;
+                major=num[i];
+            }else if(major==num[i]){
+                count++;
+            }else count--;
+        }
+        return major;
+    }
