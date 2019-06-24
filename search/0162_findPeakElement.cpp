6b6ace56084a7e9996c45c09099530a9e3f5ff27
zephyr-bj <zephyrutd@gmail.com>
2019-06-23 17:57:31 -0700

    Create 0162_findPeakElement.cpp

---
+++ search/0162_findPeakElement.cpp
@@ -0,0 +1,23 @@
+    int findPeakUtil(vector<int>& arr, int low, int high, int n){
+        int mid = low + (high - low)/2;  /* (low + high)/2 */
+  
+        // Compare middle element with its neighbours (if neighbours exist) 
+        if ((mid == 0 || arr[mid-1] <= arr[mid]) && 
+            (mid == n-1 || arr[mid+1] <= arr[mid])) 
+            return mid; 
+  
+        // If middle element is not peak and its left neighbour is greater  
+        // than it, then left half must have a peak element 
+        else if (mid > 0 && arr[mid-1] > arr[mid]) 
+            return findPeakUtil(arr, low, (mid -1), n); 
+  
+        // If middle element is not peak and its right neighbour is greater 
+        // than it, then right half must have a peak element 
+        else return findPeakUtil(arr, (mid + 1), high, n);         
+    }
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
+        return findPeakUtil(nums, low, high, n); 
+    }
