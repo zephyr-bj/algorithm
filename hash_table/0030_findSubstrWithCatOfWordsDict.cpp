1a5364262ecc95cdcb6e5f5ccbe25656c1399f93
zephyr-bj <zephyrutd@gmail.com>
2019-06-24 17:15:03 -0700

    Update 0030_findSubstrWithCatOfWordsDict.cpp

--- hash_table/0030_findSubstrWithCatOfWordsDict.cpp
+++ hash_table/0030_findSubstrWithCatOfWordsDict.cpp
@@ -1,37 +1,44 @@
-    vector<int> findSubstring(string s, vector<string>& words) {
-        unordered_map<string,int>tofind;
-        int m=words.size();
-        vector<int>ans;
-        if(m==0)return ans;
-        for(int i=0; i<m; i++){
-            if(tofind.find(words[i])!=tofind.end())tofind[words[i]]+=1;
-            else tofind[words[i]]=1;
-        }
-        int L = s.size();
-        int n=words[0].size();
-        for(int j=0; j<L-(n*m-1); j++){
-            unordered_map<string,int>found;
-            int sum=0;
-            for(int k=j; k<L-(n-1); k+=n){
-                string tmp = s.substr(k,n);
-                if(tofind.find(tmp)!=tofind.end()){
-                    if(found.find(tmp)==found.end()){
-                        found[tmp]=1;
-                        sum++;
-                    }else{
-                        if(found[tmp]<tofind[tmp]){
-                            found[tmp]+=1;
-                            sum++;
-                        }else{
-                            break;
-                        }
-                    }
-                }else{
-                    break;
-                }
-                if(sum==m)break;
+    vector<int> findSubstring(string S, vector<string> &L) {
+        vector<int> result;
+        if(L.size()<1)return result;
+        int L0 = L[0].size();
+        int L1 = L.size()*L0;
+        int L2 = S.size();
+        if(L2<1)return result;
+        if(L1>L2) return result;
+        
+        map<string,int> toFind,hasFound;
+        for(int i=0; i<L.size(); i++){
+            if(toFind.find(L[i])==toFind.end()){
+                toFind[L[i]]=1;
             }
-            if(sum==m)ans.push_back(j);
+            else{
+                toFind[L[i]]++;
+            }
         }
-        return ans;
+        
+        for(int k = 0; k<L0; k++){
+            for(int j=0; j<L.size(); j++)hasFound[L[j]]=0;
+            int cnt = 0;
+            int i = k;
+            while(i+L0<=L2){
+                string t1 = S.substr(i,L0); 
+                if(toFind.find(t1)!=toFind.end()){
+                    hasFound[t1]++;  
+                    if(hasFound[t1] <= toFind[t1])cnt++;
+                }
+                if(cnt==L.size()){
+                     result.push_back(i+L0-L1);
+                }
+                if(i+L0 >= L1 && i+L0+L0 <= L2){
+                    string t2 = S.substr(i+L0-L1,L0);
+                    if(toFind.find(t2)!=toFind.end()){
+                        hasFound[t2]--;
+                        if(hasFound[t2] < toFind[t2])cnt--;
+                    }
+                }
+                i+=L0;
+            }
+        }
+        return result;
     }
