3bfa45784aa83cab36cf30225a6e46611b05653a
zephyr-bj <zephyrutd@gmail.com>
2019-08-24 16:28:19 -0700

    Update 0199_btRightSideView.cpp

--- tree/0199_btRightSideView.cpp
+++ tree/0199_btRightSideView.cpp
@@ -14,3 +14,18 @@
         }
         return ans;
     }
+
+class Solution {
+public:
+    void tool(TreeNode * root, vector<int>&ans, int level){
+        if(root==NULL)return;
+        if(ans.size()<=level)ans.push_back(root->val);
+        tool(root->right, ans, level+1);
+        tool(root->left, ans, level+1);
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int>ans;
+        tool(root, ans, 0);
+        return ans;
+    }
+};
