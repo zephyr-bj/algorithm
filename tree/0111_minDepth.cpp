3f6227da59e9704368386bfbf15c92e6b4925ec6
zephyr-bj <zephyrutd@gmail.com>
2019-08-24 09:29:11 -0700

    Update 0111_minDepth.cpp

--- tree/0111_minDepth.cpp
+++ tree/0111_minDepth.cpp
@@ -1,15 +1,9 @@
-    void tool(TreeNode * root, int l, int &ans){
-        if(root->left==NULL&&root->right==NULL){
-            ans=l<ans?l:ans;
-            return;
-        }
-        if(root->left!=NULL)tool(root->left,l+1,ans);
-        if(root->right!=NULL)tool(root->right,l+1, ans);
-    }
     int minDepth(TreeNode* root) {
-        int ans=INT_MAX;
         if(root==NULL)return 0;
-        int level=1;
-        tool(root,level,ans);
-        return ans;
+        int l = minDepth(root->left);
+        int r = minDepth(root->right);
+        if(l==0&&r==0)return 1;
+        else if(l==0)return r+1;
+        else if(r==0)return l+1;
+        else return 1+min(r,l);
     }
