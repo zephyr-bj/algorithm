0aa42c85e74f3afd0d605c5278d5311a890ab86c
zephyr-bj <zephyrutd@gmail.com>
2019-09-11 23:42:00 -0700

    Update 0019_removeNthFromEnd.cpp

--- list/0019_removeNthFromEnd.cpp
+++ list/0019_removeNthFromEnd.cpp
@@ -1,15 +1,19 @@
 
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode * p=head;
-        ListNode * q=head;
-        while(p->next!=NULL){
-            p=p->next;
+        if(head==NULL)return NULL;
+        ListNode dummy(0);
+        dummy.next=head;
+        ListNode * p1 = head;
+        ListNode * p2 = &dummy;
+        while(p1!=NULL){
+            if(n<=0)p2=p2->next;
+            p1=p1->next;
             n--;
-            if(n<0){
-                q=q->next;
-            }
         }
-        if(n<=0)q->next=q->next->next;
-        if(n==1)return head->next;
-        return head;
+        if(n<=0){
+            ListNode * tmp = p2->next;
+            p2->next=p2->next->next;
+            delete tmp;
+        }
+        return dummy.next;
     }
