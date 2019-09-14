02a3419d1b8052ebd0d927474a48ba0957a8f6c3
zephyr-bj <zephyrutd@gmail.com>
2019-09-14 10:08:45 -0700

    Update 0086_partitionList.cpp

--- list/0086_partitionList.cpp
+++ list/0086_partitionList.cpp
@@ -1,4 +1,26 @@
     ListNode* partition(ListNode* head, int x) {
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode * p=&dummy;
+        while(p->next!=NULL&&p->next->val<x)
+            p=p->next;
+        ListNode * cur = p->next;//initialized as large head
+        ListNode * nex = cur==NULL?NULL:cur->next;
+        while(cur!=NULL&&nex!=NULL){
+            if(nex->val<x){
+                cur->next=nex->next;
+                nex->next=p->next;
+                p->next=nex;
+                p=p->next;
+            }else{
+                cur=cur->next;
+            }
+            nex=cur->next;
+        }
+        return dummy.next;
+    }
+// 31 lines sulotion
+/* ListNode* partition(ListNode* head, int x) {
         if(head==NULL||head->next==NULL)return head;
         ListNode * lh = NULL, * lt = NULL;
         ListNode * uh = NULL, * ut = NULL;
@@ -28,4 +50,4 @@
         if(lh==NULL)return uh;
         lt->next=uh;
         return lh;
-    }
+    }*/
