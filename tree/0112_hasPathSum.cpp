74d80376becd6213be147bdcaa288cfa1c872efe
zephyr-bj <zephyrutd@gmail.com>
2019-09-17 12:58:53 -0700

    Update 0112_hasPathSum.cpp

--- tree/0112_hasPathSum.cpp
+++ tree/0112_hasPathSum.cpp
@@ -1,16 +1,9 @@
     bool hasPathSum(TreeNode* root, int sum) {
-        if(root==NULL){
-            return false;
-        }else{
-            if(root->left!=NULL){
-                if(hasPathSum(root->left,sum-root->val))return true;
-            }
-            if(root->right!=NULL){
-                if(hasPathSum(root->right,sum-root->val))return true;
-            }
-            if(root->right==NULL&&root->left==NULL){
-                if(root->val==sum)return true;
-            }
-            return false;
+        if(root==NULL)return false;
+        if(root->left==NULL&&root->right==NULL){
+            return root->val==sum;
         }
+        if(hasPathSum(root->left, sum-root->val))return true;
+        if(hasPathSum(root->right, sum-root->val))return true;
+        return false;
     }
