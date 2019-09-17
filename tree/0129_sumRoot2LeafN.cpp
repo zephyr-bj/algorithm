44c9163ea27b10c1e263091648b4dc6c333c95fd
zephyr-bj <zephyrutd@gmail.com>
2019-09-17 12:57:54 -0700

    Update 0129_sumRoot2LeafN.cpp

--- tree/0129_sumRoot2LeafN.cpp
+++ tree/0129_sumRoot2LeafN.cpp
@@ -1,22 +1,11 @@
-    void tool(TreeNode * root, int &curr, int &sum){
-        if(root==NULL)return;
-        if(root->left!=NULL){
-            curr=curr*10+root->val;
-            tool(root->left, curr, sum);
-            curr/=10;
-        }
-        if(root->right!=NULL){
-            curr=curr*10+root->val;
-            tool(root->right, curr, sum);
-            curr/=10;
-        }
-        if(root->left==NULL&&root->right==NULL){
-            sum+=curr*10+root->val;
-        }
+    int sumTool(TreeNode * root, int sum){
+        if(root==NULL)return 0;
+        if(root->left==NULL&&root->right==NULL)
+            return 10*sum+root->val;
+        int l = sumTool(root->left, sum*10+root->val);
+        int r = sumTool(root->right, sum*10+root->val);
+        return l+r;
     }
     int sumNumbers(TreeNode* root) {
-        int curr=0;
-        int sum=0;
-        tool(root,curr,sum);
-        return sum;
+        return sumTool(root,0);
     }
