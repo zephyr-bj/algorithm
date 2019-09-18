b6494fb53e986ce587ea8fd4593f592f1cde7ad9
zephyr-bj <zephyrutd@gmail.com>
2019-09-17 23:35:34 -0700

    Update 0114_flattenTree.cpp

--- tree/0114_flattenTree.cpp
+++ tree/0114_flattenTree.cpp
@@ -1,16 +1,31 @@
+// 4ms iterative solution
     void flatten(TreeNode* root) {
-        if(root==NULL)return;
-        TreeNode* ln=root->left; root->left=NULL;
-        TreeNode* rn=root->right; root->right=NULL;
-        flatten(ln);
-        flatten(rn);
-        if(ln!=NULL){
-            TreeNode* p = ln;
-            while(p->right!=NULL)p=p->right;
-            p->right=rn;
-            root->right=ln;
-        }else{
-            root->right=rn;
+      TreeNode *cur = root;
+      while (cur) {
+        if (cur->left) {
+          TreeNode *p = cur->left;
+          while (p->right) p = p->right;
+          p->right = cur->right;
+          cur->right = cur->left;
+          cur->left = nullptr;
         }
-        return;
+        cur = cur->right;
+      }
+    }
+// 0ms recursive solution
+    void flatten(TreeNode* root) {
+        flatten(root, NULL);
+    }
+    TreeNode* flatten(TreeNode* root, TreeNode* pre) {
+        if (root == NULL) return pre;
+        // flatten the right subtree
+        pre = flatten(root->right, pre);
+        // flatten the left subtree, and assign the right subtree as right
+        pre = flatten(root->left, pre);
+        // remove left
+        root->left = NULL;
+        // assign the flatten left subtree as right
+        root->right = pre;
+        // return self as a flattened tree 
+        return root;
     }
