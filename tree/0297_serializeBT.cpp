45f3da3ca39827c9105d2d93b6ebf7eb17753e29
zephyr-bj <zephyrutd@gmail.com>
2019-09-17 22:30:38 -0700

    Update 0297_serializeBT.cpp

--- tree/0297_serializeBT.cpp
+++ tree/0297_serializeBT.cpp
@@ -3,51 +3,47 @@ public:
 
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        string ans;
         queue<TreeNode*>bin;
         bin.push(root);
+        string ans;
         while(!bin.empty()){
-            int n = bin.size();
-            for(int i=0; i<n; i++){
-                TreeNode*p=bin.front();
-                bin.pop();
-                if(!ans.empty())ans+=",";
-                if(p!=NULL){
-                    ans+=to_string(p->val);
-                    bin.push(p->left);
-                    bin.push(p->right);
-                }else{
-                    ans+="null";
-                }
+            if(ans.size()!=0)ans+=",";
+            if(bin.front()==NULL){
+                ans+="null";
+            }else{
+                ans+=to_string(bin.front()->val);
+                bin.push(bin.front()->left);
+                bin.push(bin.front()->right);
             }
+            bin.pop();
         }
         return ans;
     }
 
     // Decodes your encoded data to tree.
-TreeNode* deserialize(string data) {
-    int n = data.size();
-    int j=0;
-    TreeNode dummy(0);
-    queue<TreeNode**>bin;
-    bin.push(&(dummy.right));
-    for(int i=0; i<=n; i++){
-        if(data[i]==','||i==n){
-            string ss = data.substr(j,i-j);
-            if(ss.compare("null")!=0){
-                TreeNode* node=new TreeNode(atoi(ss.c_str()));
-                *(bin.front())=node;
-                bin.push(&(node->left));
-                bin.push(&(node->right));
+    TreeNode* deserialize(string data) {
+        TreeNode * root=NULL;
+        queue<TreeNode**>bin;
+        bin.push(&root);
+        int x = 0; int y=0; int n = data.size();
+        while(y<=n){
+            if(data[y]==','||y==n){
+                string str = data.substr(x,y-x);
+                if(str.compare("null")!=0){
+                   TreeNode * node = new TreeNode(atoi(str.c_str()));
+                    *bin.front()=node;
+                    bin.push(&(node->left));
+                    bin.push(&(node->right));
+                }
+                bin.pop();
+                x=y+1;
             }
-            bin.pop();
-            j=i+1;
+            y++;
         }
+        return root;
     }
-    return dummy.right;
-}
-    
 };
+
 /*
 BST, use queue, 
 construct a tree, use the address of pointer "TreeNode**"
