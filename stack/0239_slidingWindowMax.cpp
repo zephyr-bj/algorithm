f3cf7c9240d40795c7291f427c6360d491dd5e06
zephyr-bj <zephyrutd@gmail.com>
2019-09-20 00:15:27 -0700

    Update 0239_slidingWindowMax.cpp

--- stack/0239_slidingWindowMax.cpp
+++ stack/0239_slidingWindowMax.cpp
@@ -1,16 +1,12 @@
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n = nums.size(); 
-        list<pair<int,int>>w;
+        list<int>bin;
+        int n = nums.size();
         vector<int>ans;
         for(int i=0; i<n; i++){
-            while(!w.empty()&&w.back().second<nums[i]){
-                w.pop_back();
-            }
-            w.push_back(pair<int,int>(i,nums[i]));
-            if(i+1>=k){
-                ans.push_back(w.front().second);
-                if(w.front().first<=i+1-k)w.pop_front();
-            }
+            while(!bin.empty()&&nums[bin.back()]<nums[i])bin.pop_back();
+            bin.push_back(i);
+            if(i>=k&&bin.front()<=i-k)bin.pop_front();
+            if(i>=k-1)ans.push_back(nums[bin.front()]);
         }
         return ans;
     }
