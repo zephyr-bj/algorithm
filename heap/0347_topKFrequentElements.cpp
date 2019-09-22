d617882a1e6bc8437d055a93c4bb0ccb6958dcff
zephyr-bj <zephyrutd@gmail.com>
2019-09-22 10:08:19 -0700

    Update 0347_topKFrequentElements.cpp

--- heap/0347_topKFrequentElements.cpp
+++ heap/0347_topKFrequentElements.cpp
@@ -19,3 +19,32 @@
         }
         return ans;
     }
+
+// use min heap
+    struct my_cmp {
+        bool operator () (pair<int,int>&a, pair<int,int>&b){return a.second > b.second;}
+    };
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int >bin;
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            if(bin.find(nums[i])==bin.end())bin[nums[i]]=1;
+            else bin[nums[i]]+=1;
+        }
+        priority_queue<pair<int,int>, vector<pair<int,int>>, my_cmp>hp;
+        for(unordered_map<int,int>::iterator it = bin.begin(); it!=bin.end(); it++){
+            if(hp.size()<k)hp.push(*it);
+            else{
+                if(it->second>hp.top().second){
+                    hp.pop();
+                    hp.push(*it);
+                }
+            }
+        }
+        vector<int>ans;
+        while(!hp.empty()){
+            ans.push_back(hp.top().first);
+            hp.pop();
+        }
+        return ans;
+    }
