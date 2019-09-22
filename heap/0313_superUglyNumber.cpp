1953c7549b2ab172c5b80f8eefd5580f51f43974
zephyr-bj <zephyrutd@gmail.com>
2019-09-22 00:33:56 -0700

    Update 0313_superUglyNumber.cpp

--- heap/0313_superUglyNumber.cpp
+++ heap/0313_superUglyNumber.cpp
@@ -30,3 +30,28 @@
     For compare function type: to define a struct with an operator () return bool
     */
     
+//iterative way below, faster and ligher
+int nthSuperUglyNumber(int n, vector<int>& primes) {
+        int k = primes.size();
+        if(n<1||k<1)return 0;
+        if(n==1)return 1;
+        vector<int>index(k,0);
+        vector<int>result(1,1);
+        while(result.size()<n){
+            int tmp = INT_MAX;
+            int idx = -1;
+            for(int i=0; i<k; i++){
+                int x = primes[i]*result[index[i]];
+                if(tmp>x){
+                    idx = i; 
+                    tmp=x;
+                }
+            }
+            index[idx]+=1;
+            result.push_back(tmp);
+            for(int i=idx+1; i<k; i++){
+                if(tmp==primes[i]*result[index[i]])index[i]+=1;
+            }
+        }
+        return result[n-1];
+    }
