38a2a216c9c2575d2633ccbbd7f0bc475a403c2e
zephyr-bj <zephyrutd@gmail.com>
2019-09-22 21:35:34 -0700

    Update 0352_disjointIntervals.cpp

--- ordered_set/0352_disjointIntervals.cpp
+++ ordered_set/0352_disjointIntervals.cpp
@@ -1,57 +1,47 @@
 class SummaryRanges {
 public:
     /** Initialize your data structure here. */
-    map<int,int>bin;
+    struct my_cmp {
+        bool operator () (const pair<int,int>&a, const pair<int,int>&b) const {
+            return a.second < b.first;
+        }
+    };
+    typedef set<pair<int,int>, my_cmp> si;
+    si seg;
     SummaryRanges() {
         
     }
     
     void addNum(int val) {
-        map<int,int>::iterator it=bin.lower_bound(val);
-        if(it==bin.end()){
-            if(bin.size()==0){
-                bin[val]=val;
-            }else{
-                if(val-1==(--bin.end())->second){
-                    (--bin.end())->second=val;
-                }else if(val-1>(--bin.end())->second){
-                    bin[val]=val;
-                }
-            }
-        }else{
-            map<int,int>::iterator itt = it;
-            itt--;
-            if(val+1==it->first){
-                if(it==bin.begin()){
-                    bin[val]=it->second;
-                }else{
-                    if(val-1==itt->second){
-                        itt->second=it->second;
-                    }else{
-                        bin[val]=it->second;
-                    }
-                }
-                bin.erase(it);
-            }else if(val+1 < it->first){
-                if(it==bin.begin()){
-                    bin[val]=val;
-                }else{
-                    if(val-1==itt->second){
-                        itt->second=val;
-                    }else if(val-1>itt->second){
-                        bin[val]=val;
-                    }
-                }
+        pair<int,int>interval({val,val});
+        si::iterator it1 = seg.lower_bound(interval);
+        if(it1!=seg.end()){
+            if(val>=it1->first&&val<=it1->second)return;
+            if(val+1==it1->first){
+                interval.second = it1->second;
+// it = seg.erase(it); if do not use the return value and assign back, it will be NULL;
+                // now it points to the following position
+                it1=seg.erase(it1);
             }
         }
+        if(it1!=seg.begin()){
+// need to assigne it2 here, if assign it2 at the beginning, it2 points to weeds after the erase above
+            si::iterator it2 = it1;
+            it2--;
+            if(val>=it2->first&&val<=it2->second)return;
+            if(val-1==it2->second){
+                interval.first = it2->first;
+                seg.erase(it2);
+            }
+        }
+        seg.insert(interval);
     }
     
     vector<vector<int>> getIntervals() {
         vector<vector<int>>ans;
-        for(map<int,int>::iterator it=bin.begin(); it!=bin.end(); it++){
-            vector<int>interval(1,it->first);
-            interval.push_back(it->second);
-            ans.push_back(interval);
+        for(auto x:seg){
+            vector<int>node({x.first, x.second});
+            ans.push_back(node);
         }
         return ans;
     }
