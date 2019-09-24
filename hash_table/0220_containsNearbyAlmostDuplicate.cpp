b3b6b3afbfb91ce430dbe2335db80bc49be945fa
zephyr-bj <zephyrutd@gmail.com>
2019-09-23 23:16:36 -0700

    Update 0220_containsNearbyAlmostDuplicate.cpp

--- hash_table/0220_containsNearbyAlmostDuplicate.cpp
+++ hash_table/0220_containsNearbyAlmostDuplicate.cpp
@@ -1,4 +1,5 @@
-    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+//bucket sort solution   
+bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
         if(t<0)return false;
         unordered_map<int,int>bin;
         int n = nums.size();
@@ -20,28 +21,16 @@
         }
         return false;
     }
-    //12ms
-    //28ms solution below
-        bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
-        if(k<1||t<0)return false;
-        unordered_map<int,vector<int>>bin;
+// set solution
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+        multiset<long long>bin;
+        if(k<1&&t<0)return false;
         int n = nums.size();
         for(int i=0; i<n; i++){
-            int target = t>0?nums[i]/t:nums[i];
-            int r = t>0?nums[i]%t:0;
-            if(bin.find(target)!=bin.end()){
-                if(i-bin[target][0]<=k&&abs(r-bin[target][1])<=t)return true;
-            }
-            if(t>0&&bin.find(target-1)!=bin.end()){
-                if(i-bin[target-1][0]<=k&&r<=bin[target-1][1])return true;
-            }
-            if(t>0&&bin.find(target+1)!=bin.end()){
-                if(i-bin[target+1][0]<=k&&r>=bin[target+1][1])return true;
-            }
-            vector<int>tmp(1,i); 
-            tmp.push_back(r);
-            bin[target]=tmp;
-            if(i>=k)bin.erase(t>0?nums[i-k]/t:nums[i-k]);
+            if(i>k)bin.erase(bin.find(nums[i-k-1]));
+            multiset<long long>::iterator it = bin.lower_bound((long long)nums[i]-(long long)t);
+            if(it!=bin.end()&&abs(nums[i]-(*it))<=t)return true;
+            bin.insert(nums[i]);
         }
         return false;
     }
