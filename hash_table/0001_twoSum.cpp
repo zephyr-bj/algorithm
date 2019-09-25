c825865d1859b415fcfbcff1d7602923653165a1
zephyr-bj <zephyrutd@gmail.com>
2019-09-24 21:04:31 -0700

    Update 0001_twoSum.cpp

--- hash_table/0001_twoSum.cpp
+++ hash_table/0001_twoSum.cpp
@@ -1,16 +1,14 @@
     vector<int> twoSum(vector<int>& nums, int target) {
-        int L = nums.size();
-        map<int,int>data;
-        vector<int>result(2,0);
-        for(int i=0; i<L; i++){
-            map<int,int>::iterator it=data.find(target-nums[i]);
-            if(it==data.end()){
-                data[nums[i]]=i;
-            }else{
-                result[0]=it->second;
-                result[1]=i;
+        unordered_map<int,int>bin;
+        vector<int>ans;
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            if(bin.find(nums[i])==bin.end())bin[target-nums[i]]=i;
+            else{
+                ans.push_back(bin[nums[i]]);
+                ans.push_back(i);
                 break;
             }
         }
-        return result;
+        return ans;
     }
