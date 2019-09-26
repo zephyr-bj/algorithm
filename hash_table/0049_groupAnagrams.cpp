3d8b0fc0e722f70edaaf1d36b9df6dd2499fbf6c
zephyr-bj <zephyrutd@gmail.com>
2019-09-25 21:57:10 -0700

    Update 0049_groupAnagrams.cpp

--- hash_table/0049_groupAnagrams.cpp
+++ hash_table/0049_groupAnagrams.cpp
@@ -12,3 +12,39 @@
         }
         return anagrams;
     }
+// 24ms solution
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        int prime[26];
+        int count=0;
+        int flag;
+        for(int i=2;i<200;i++){
+            flag=0;
+            for(int j=2;j<i;j++){
+                if(i%j==0){
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0){
+                count++;
+                prime[count-1]=i;
+                //cout<<prime[count-1]<<" ";
+            }           
+            if(count==26)break;
+        }
+        vector<vector<string> >res;
+        unordered_map<int,int>m;
+        for(auto str:strs){
+            unsigned int val=1;
+            for(int i=0;i<str.size();i++){
+                val*=prime[str[i]-'a'];
+            }
+            if(m.find(val)==m.end()){
+                m[val]=res.size();
+                res.push_back(vector<string> ({str}));
+            }else{
+                res[m[val]].push_back(str);
+            }
+        }
+        return res;
+    }
