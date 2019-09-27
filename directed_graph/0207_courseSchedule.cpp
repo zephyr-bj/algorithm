db6e2a7226e2811114c035b2feafcef57378e1ca
zephyr-bj <zephyrutd@gmail.com>
2019-09-27 13:01:18 -0700

    Update 0207_courseSchedule.cpp

--- directed_graph/0207_courseSchedule.cpp
+++ directed_graph/0207_courseSchedule.cpp
@@ -27,42 +27,28 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int,vector<int>>depends;
-        vector<int>visiting(numCourses, 0);
-        int n = prerequisites.size();
-        for(int i=0; i<n; i++){
-            int src=prerequisites[i][1];
-            int dst=prerequisites[i][0];
-            if(depends.find(src)==depends.end()){
-                vector<int>edges(1,dst);
-                depends[src]=edges;
-            }else{
-                depends[src].push_back(dst);
-            }
-            visiting[dst]+=1;
+        unordered_set<int>learned;
+        for(int i=0; i<numCourses; i++)learned.insert(i);
+        vector<vector<int>>leadsto(numCourses,vector<int>({}));
+        vector<int>dependson(numCourses,0);
+        for(auto v:prerequisites){
+            dependson[v[0]]+=1;
+            leadsto[v[1]].push_back(v[0]);
+            learned.erase(v[0]);
         }
-        queue<int>unlock;
-        int learned = 0;
-        for(int i=0; i<numCourses; i++){
-            if(visiting[i]==0){
-                unlock.push(i); learned+=1;
-            }
-        }
-        while(!unlock.empty()){
-            int l = unlock.size();
-            for(int i=0; i<l; i++){
-                int c = unlock.front();
-                unlock.pop();
-                for(int j=0; j<depends[c].size(); j++){
-                    int dst=depends[c][j];
-                    visiting[dst]-=1;
-                    if(visiting[dst]==0){
-                        unlock.push(dst); learned+=1;
-                    }
+        int cnt = learned.size();
+        while(!learned.empty()){
+            int x = *learned.begin();
+            learned.erase(learned.begin());
+            for(auto y : leadsto[x]){
+                dependson[y]--;
+                if(dependson[y]==0){
+                    learned.insert(y);
+                    cnt++;
                 }
             }
         }
-        return learned==numCourses;
+        return cnt==numCourses;
     }
 };
 */
