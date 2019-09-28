09ea080ff87d1845405318c076285428f5d35174
zephyr-bj <zephyrutd@gmail.com>
2019-09-28 12:16:12 -0700

    Update 0079_wordSearch.cpp

--- directed_graph/0079_wordSearch.cpp
+++ directed_graph/0079_wordSearch.cpp
@@ -1,38 +1,24 @@
-    bool existTool(vector<vector<char>>&board, int a, int b, string & word, int k){
-        if(k==word.size())return true;
-        int n=board.size(); int m=board[0].size();
-        if(a>0&&board[a-1][b]==word[k]){
-            board[a-1][b]=0;
-            if(existTool(board,a-1,b,word,k+1))return true;
-            board[a-1][b]=word[k];
-        }
-        if(a<n-1&&board[a+1][b]==word[k]){
-            board[a+1][b]=0;
-            if(existTool(board,a+1,b,word,k+1))return true;
-            board[a+1][b]=word[k];
-        }
-        if(b>0&&board[a][b-1]==word[k]){
-            board[a][b-1]=0;
-            if(existTool(board,a,b-1,word,k+1))return true;
-            board[a][b-1]=word[k];
-        }
-        if(b<m-1&&board[a][b+1]==word[k]){
-            board[a][b+1]=0;
-            if(existTool(board,a,b+1,word,k+1))return true;
-            board[a][b+1]=word[k];
-        }
+    bool dfs(vector<vector<char>>&board, int i, int j, int n, int m,
+             string &word, int k, int l){
+        if(k==l)return true;
+        board[i][j]='*';
+        if(i>0&&board[i-1][j]==word[k]&&dfs(board,i-1,j,n,m,word,k+1,l))return true;
+        if(i<n-1&&board[i+1][j]==word[k]&&dfs(board,i+1,j,n,m,word,k+1,l))return true;
+        if(j>0&&board[i][j-1]==word[k]&&dfs(board,i,j-1,n,m,word,k+1,l))return true;
+        if(j<m-1&&board[i][j+1]==word[k]&&dfs(board,i,j+1,n,m,word,k+1,l))return true;
+        board[i][j]=word[k-1];
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
         int n=board.size();
-        int m=board[0].size();
+        if(n<1)return false;
+        int m = board[0].size();
+        if(m<1)return false;
+        int l = word.size();
+        if(l<1)return false;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(board[i][j]==word[0]){
-                    board[i][j]=0;
-                    if(existTool(board,i,j,word,1))return true;
-                    board[i][j]=word[0];
-                }
+                if(board[i][j]==word[0]&&dfs(board,i,j,n,m,word,1,l))return true;
             }
         }
         return false;
