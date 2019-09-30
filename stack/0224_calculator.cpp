3ebd589b126ad6a0d196ee6204bcf96d303e04f5
zephyr-bj <zephyrutd@gmail.com>
2019-09-29 18:54:44 -0700

    Update 0224_calculator.cpp

--- stack/0224_calculator.cpp
+++ stack/0224_calculator.cpp
@@ -2,33 +2,30 @@
         int n = s.size();
         stack<int>res({0});
         stack<char>sign({'+'});
-        int a = 0;
+        int num = 0;
         int i =0;
-        while(i<n){
-            if(s[i]>='0'&&s[i]<='9'){
-                int j = i+1;
-                while(j<n&&s[j]>='0'&&s[j]<='9')j++;
-                int b = atoi(s.substr(i,j-i).c_str());
-                if(sign.top()=='+')res.top()+=b;
-                else res.top()-=b;
-                i=j;
-            }else if(s[i]=='+'||s[i]=='-'){
-                sign.top() = s[i];
-                i++;
-            }else if(s[i]=='('){
-                res.push(0);
-                sign.push('+');
-                i++;
-            }else if(s[i]==')'){
-                int b = res.top();
-                res.pop();
-                sign.pop();
-                if(sign.top()=='+')res.top()+=b;
-                else res.top()-=b;
-                i++;
-            }else{
-                i++;
-            }
+        for(int i=0; i<=n; i++){
+            if(i<n&&s[i]>='0'&&s[i]<='9'){
+                num=num*10+(s[i]-'0');
+            }else if(i==n||s[i]=='+'||s[i]=='-'||s[i]=='('||s[i]==')'){
+                if(i==n||s[i]=='+'||s[i]=='-'){
+                    if(sign.top()=='+')res.top()+=num;
+                    else res.top()-=num;
+                    sign.top() = s[i];
+                }else if(s[i]=='('){
+                    res.push(0);
+                    sign.push('+');
+                }else if(s[i]==')'){
+                    if(sign.top()=='+')res.top()+=num;
+                    else res.top()-=num;
+                    int x = res.top();
+                    res.pop();
+                    sign.pop();
+                    if(sign.top()=='+')res.top()+=x;
+                    else res.top()-=x;
+                }
+                num=0;
+            } 
         }
         return res.top();
     }
