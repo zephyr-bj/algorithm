c5af4d610622ec604396765324288cd4f1c890e3
zephyr-bj <zephyrutd@gmail.com>
2019-10-07 17:31:09 -0700

    Update 0064_minPathSum.cpp

--- dp/0064_minPathSum.cpp
+++ dp/0064_minPathSum.cpp
@@ -15,3 +15,23 @@
         }
         return grid[m-1][n-1];
     }
+
+// use one dimension dp
+    int minPathSumII(vector<vector<int>>& grid) {
+        int n = grid.size();
+        if(n<1)return 0;
+        int m = grid[0].size();
+        if(m<1)return 0;
+        vector<int>sum(m,0);
+        sum[0]=grid[0][0];
+        for(int j=1; j<m; j++){
+            sum[j]=grid[0][j]+sum[j-1];
+        }
+        for(int i=1; i<n; i++){
+            sum[0]=sum[0]+grid[i][0];
+            for(int j=1; j<m; j++){
+                sum[j]=min(sum[j],sum[j-1])+grid[i][j];
+            }
+        }
+        return sum[m-1];
+    }
