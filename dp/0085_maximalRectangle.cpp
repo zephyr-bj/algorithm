df70e08c921834634cea9238fc3b2b1b481dd67e
zephyr-bj <zephyrutd@gmail.com>
2019-10-07 22:24:24 -0700

    Update 0085_maximalRectangle.cpp

--- dp/0085_maximalRectangle.cpp
+++ dp/0085_maximalRectangle.cpp
@@ -26,3 +26,32 @@
     1. partial optimal structure per row, 
     2. normal order
     */
+// one dimension solution
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int n = matrix.size();
+        if(n<1)return 0;
+        int m = matrix[0].size();
+        if(m<1)return 0;
+        vector<int>h(m,0);
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j]=='1'){
+                    h[j]+=1;
+                    int x = h[j];
+                    int area = h[j];
+                    for(int k=j-1; k>=0; k--){
+                        if(h[k]==0)break;
+                        else {
+                            x = min(x,h[k]);
+                            area = max(area,x*(j-k+1));
+                        }
+                    }
+                    ans = max(area,ans);
+                }else{
+                    h[j]=0;
+                }
+            }
+        }
+        return ans;
+    }
