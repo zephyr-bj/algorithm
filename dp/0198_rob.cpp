3047c9008eb7f056d90cc333074692e819c3cf99
zephyr-bj <zephyrutd@gmail.com>
2019-10-08 23:52:35 -0700

    Update 0198_rob.cpp

--- dp/0198_rob.cpp
+++ dp/0198_rob.cpp
@@ -9,3 +9,17 @@
         }
         return bin.back();
     }
+// time O(n), space O(1)
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n<1)return 0;
+        int b2 = nums[0];
+        if(n<2)return b2;
+        int b1 = max(b2,nums[1]);
+        for(int i=2; i<n; i++){
+            int x = nums[i]+b2;
+            b2 = max(b2,b1);
+            b1 = max(b1,x);
+        }
+        return b1;
+    }
