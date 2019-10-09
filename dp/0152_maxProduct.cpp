e49a5de8529f9bc6d3e9b51845ee3839a73f6056
zephyr-bj <zephyrutd@gmail.com>
2019-10-08 22:33:42 -0700

    Update 0152_maxProduct.cpp

--- dp/0152_maxProduct.cpp
+++ dp/0152_maxProduct.cpp
@@ -1,35 +1,27 @@
+    // Time O(n), Space O(1)
     int maxProduct(vector<int>& nums) {
-        int n=nums.size();
-        int i=0; 
-        while(i<n&&nums[i]==0)i++;
-        long long ans=i<n?nums[i]:0;
-        long long maxP=i<n?nums[i]:0;
-        long long minP=i<n?nums[i]:0;
-        i++;
-        while(i<n){
-            if(nums[i]==0){
-                if(i<n-1){
-                    maxP=nums[i+1]; 
-                    minP=nums[i+1];
-                }
-                if(ans<0)ans=0;
-                i++;
-            }else {
-                long long n1=maxP*nums[i];
-                long long n2=minP*nums[i];
-                maxP=nums[i];
-                if(maxP<n1)maxP=n1;
-                if(maxP<n2)maxP=n2;
-                minP=nums[i];
-                if(minP>n1)minP=n1;
-                if(minP>n2)minP=n2;
+        int n = nums.size();
+        if(n<1)return 0;
+        int minP = nums[0];
+        int maxP = nums[0];
+        int ans = nums[0];
+        for(int i=1; i<n; i++){
+            if(nums[i]>0){
+                minP=min(nums[i],minP*nums[i]);
+                maxP=max(nums[i],maxP*nums[i]);
+            }else if(nums[i]<0){
+                // this temperary variable is important
+                int minCP = minP; 
+                minP=min(nums[i],maxP*nums[i]);
+                maxP=max(nums[i],minCP*nums[i]);
+            }else{
+                minP=0; maxP=0;
             }
-            if(ans<maxP)ans=maxP;
-            i++;
+            ans = max(ans,maxP);
         }
-        return (int)(ans);
+        return ans;
     }
     /*
-    need to variables, maxP and minP, 
+    need two variables, maxP and minP, 
     important is definition: max/min product of the subarray including the ith num
     */
