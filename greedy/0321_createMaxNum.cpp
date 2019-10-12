a0418328a497d8d33839ef3e48962e5457fd98b3
zephyr-bj <zephyrutd@gmail.com>
2019-10-11 20:47:05 -0700

    Update 0321_createMaxNum.cpp

--- greedy/0321_createMaxNum.cpp
+++ greedy/0321_createMaxNum.cpp
@@ -1,47 +1,49 @@
-    /*vector<int> maxNumber(vector<int>& nums, int k){
-        vector<int> ans;                
-        for (int i = 0; i < nums.size(); ++i) {
-            while (!ans.empty() && nums[i] > ans.back() 
-                   && (ans.size()-1) + (nums.size() - i) >= k) ans.pop_back();
-            if (ans.size() < k) ans.push_back(nums[i]);
-        }
-        return ans;
-    }*/
-    vector<int> maxNumber(vector<int>& nums, int k){
-        vector<int> ans(k,0);                
-        int j = 0;
-        for (int i = 0; i < nums.size(); ++i) {
-            while (j > 0 && nums[i] > ans[j - 1] 
-                   && nums.size() - i > k - j) --j;
-            if (j < k) ans[j++] = nums[i];
-        }
-        return ans;
-    }
-    bool compare(vector<int> &nums1, int i, vector<int> &nums2, int j) {
-        while (i < nums1.size() && j < nums2.size() && nums1[i] == nums2[j]) {
-            ++i;++j;
-        }
-        if (j == nums2.size()) return true;
-        if (i < nums1.size() && nums1[i] > nums2[j]) return true;
-        return false;
-    }
-    vector<int> merge(vector<int> &nums1, vector<int> &nums2, int k) {
-        vector<int> res(k, 0);
-        for (int i = 0, j = 0, r = 0; r < k; ++r) {
-            res[r] = compare(nums1, i, nums2, j) ? nums1[i++] : nums2[j++];
-        }
-        return res;
-    }
-    vector<int> maxNumber(vector<int>& nums1, vector<int>& nums2, int k) {
+    vector<int>maxNumber(vector<int>&nums, int k){
+        int n = nums.size();
+        int i = 0; int l = k;
         vector<int>ans(k,0);
+        for(int t=0; t<k; t++){
+            for(int j=i; j<=n-l; j++){
+                if(nums[i]<nums[j])i=j;
+            }
+            ans[t]=nums[i]; i++;l--;
+        }
+        return ans;
+    }
+    vector<int> maxNumberMerge(vector<int>&nums1, vector<int>&nums2){
         int n = nums1.size();
         int m = nums2.size();
-        for(int i=0;i<=min(k,n);i++){
+        int i=0; int j=0;
+        vector<int>ans;
+        while(i<n||j<m){
+            if(maxNumberCompare(nums1,i,nums2,j)){
+                ans.push_back(nums1[i]); i++;
+            }else{
+                ans.push_back(nums2[j]); j++;
+            }
+        }
+        return ans;
+    }
+    bool maxNumberCompare(vector<int>&nums1, int i, vector<int>& nums2, int j){
+        int n = nums1.size();
+        int m = nums2.size();
+        while(i<n && j<m && nums1[i]==nums2[j]){
+            i++; j++;
+        }
+        if(i==n)return false;
+        else if(j==m)return true;
+        else return nums1[i]>nums2[j];
+    }
+    vector<int> maxNumber(vector<int>& nums1, vector<int>& nums2, int k) {
+        int n = nums1.size();
+        int m = nums2.size();
+        vector<int>ans(k,0);
+        for(int i=0; i<=min(n,k); i++){
             if(k-i>m)continue;
-            vector<int>x=maxNumber(nums1,i);
-            vector<int>y=maxNumber(nums2,k-i);
-            vector<int>z = merge(x,y,k);
-            if(compare(z,0,ans,0))ans=z;
+            vector<int>num1 = maxNumber(nums1,i);
+            vector<int>num2 = maxNumber(nums2,k-i);
+            vector<int>num12 = maxNumberMerge(num1,num2);
+            if(maxNumberCompare(num12,0,ans,0))ans=num12;
         }
         return ans;
     }
