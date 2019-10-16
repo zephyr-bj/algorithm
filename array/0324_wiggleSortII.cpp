28c225fbd5a8d6990b71be653656aa781158bb16
zephyr-bj <zephyrutd@gmail.com>
2019-10-15 22:13:09 -0700

    Update 0324_wiggleSortII.cpp

--- array/0324_wiggleSortII.cpp
+++ array/0324_wiggleSortII.cpp
@@ -89,3 +89,37 @@ i=2, j=4, k=5    1, 4, 1, 6, 5, 1,
 i=2, j=5, k=5    1, 4, 1, 6, 5, 1, 
                  1, 4, 1, 6, 1, 5
 */
+/*
+(1) elements smaller than the 'median' are put into the last even slots
+(2) elements larger than the 'median' are put into the first odd slots
+(3) the medians are put into the remaining slots.
+*/
+public void wiggleSortX(int[] nums) {
+    int median=findKthLargest(nums,(nums.length+1)/2);
+    int odd=1;
+    int even=nums.length%2==0?nums.length-2:nums.length-1;
+    int[] tmpArr=new int[nums.length];
+    for(int i=0;i<nums.length;i++){
+        if(nums[i]>median){
+            tmpArr[odd]=nums[i];
+            odd+=2;
+            continue;
+        }
+        if(nums[i]<median){
+            tmpArr[even]=nums[i];
+            even-=2;
+            continue;
+        }
+    }
+    while(odd<nums.length){
+        tmpArr[odd]=median;
+        odd+=2;
+    }
+    while(even>=0){
+        tmpArr[even]=median;
+        even-=2;
+    }
+    for(int i=0;i<nums.length;i++){
+        nums[i]=tmpArr[i];
+    }
+}
