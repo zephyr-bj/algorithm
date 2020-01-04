5d3bd714354b6af1de83f676bcb263764c08e883
zephyr-bj <zephyrutd@gmail.com>
2020-01-03 23:00:47 -0800

    Update 0220_containsNearbyAlmostDuplicate.cpp

--- hash_table/0220_containsNearbyAlmostDuplicate.cpp
+++ hash_table/0220_containsNearbyAlmostDuplicate.cpp
@@ -1,36 +1,42 @@
 //bucket sort solution   
+//for the bucket sort solution, I'm stuck at a case such as k=5, t=3, nums = {3, 1, 5}
+//I'm think the newer but further element, such as 1 will overwrite the older but closer element 3, so 5 has no chance to see 3.
+//But this kind of case is not going to happenm, since 1 and 3 will fall in the same bucket and return true, so we do not need 5
+
+// the "getBucketId" function deal with the negative cases. 
+long long getBucketId(long long i, long long w) {
+    return i < 0 ? (i + 1) / w - 1 : i / w;
+}
 bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
-        if(t<0)return false;
-        unordered_map<int,int>bin;
-        int n = nums.size();
-        for(int i=0; i<n; i++){
-            int target = t>0?nums[i]/t:nums[i];
-            int r = t>0?nums[i]%t:0;
-            if(bin.find(target)!=bin.end()){
-                if(abs(r-bin[target])<=t)return true;
-            }else if(t>0){
-                if(bin.find(target-1)!=bin.end()){
-                    if(r<=bin[target-1])return true;
-                }
-                if(bin.find(target+1)!=bin.end()){
-                    if(r>=bin[target+1])return true;
-                }
-            }
-            bin[target]=r;
-            if(i>=k)bin.erase(t>0?nums[i-k]/t:nums[i-k]);
-        }
+    int n = nums.size();
+    if (n < 2 || k < 1 || t < 0){
         return false;
     }
+    unordered_map<long long, long long> buckets;
+    long long width = (long long)t + 1;
+    for (int i = 0; i < n; i++){
+        long long id = getBucketId(nums[i], width);
+        if (buckets.find(id) != buckets.end()){
+            return true;
+        }
+        if ((buckets.find(id - 1) != buckets.end() && nums[i] - buckets[id - 1] < width)) return true;
+        if ((buckets.find(id + 1) != buckets.end() && buckets[id + 1] - nums[i] < width)) return true;
+
+        buckets[id] = nums[i];
+        if (i >= k){
+            buckets.erase(getBucketId(nums[i - k], width));
+        }
+    }
+    return false;
+}
 // set solution
-    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
-        multiset<long long>bin;
-        if(k<1&&t<0)return false;
-        int n = nums.size();
-        for(int i=0; i<n; i++){
-            if(i>k)bin.erase(bin.find(nums[i-k-1]));
-            multiset<long long>::iterator it = bin.lower_bound((long long)nums[i]-(long long)t);
-            if(it!=bin.end()&&abs(nums[i]-(*it))<=t)return true;
-            bin.insert(nums[i]);
-        }
-        return false;
+bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+    set<int> window; 
+    for (int i = 0; i < nums.size(); i++) {
+        if (i > k) window.erase(nums[i-k-1]); 
+        auto pos = window.lower_bound(nums[i] - t); //looking for elements larger or equal to (num[i]-t)  
+        if (pos != window.end() && *pos - nums[i] <= t) return true;
+        window.insert(nums[i]);
     }
+    return false;
+}
