e43db2a30bd1562f71e84fcbfd99939b278d70a0
zephyr-bj <zephyrutd@gmail.com>
2020-02-05 00:30:20 -0800

    Update 0210_courseScheduleII.cpp

--- directed_graph/0210_courseScheduleII.cpp
+++ directed_graph/0210_courseScheduleII.cpp
@@ -1,3 +1,30 @@
+/*DFS*/
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>graph(numCourses,vector<int>{});
+        for(vector<int>d:prerequisites){
+            graph[d[0]].push_back(d[1]);
+        }
+        vector<int>v(numCourses,0);
+        vector<int>s(numCourses,0);
+        vector<int>order;
+        for(int i=0; i<numCourses; i++){
+            if(isCycle(graph,v,s,i,order))return vector<int>();
+        }
+        return order;
+    }
+    bool isCycle(vector<vector<int>>&graph, vector<int>&v, vector<int>&s, int x, vector<int>&order){
+        if(!v[x]){
+            v[x]=1;
+            s[x]=1;
+            for(int i:graph[x]){
+                if(!v[i]&&isCycle(graph,v,s,i,order))return true;
+                else if(s[i])return true;
+            }
+            order.push_back(x);
+        }
+        s[x]=0;
+        return false;
+    }
 /*BFS*/
 class Solution {
 public:
