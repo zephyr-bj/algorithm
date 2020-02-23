fd0c6e67de371d038b7d8765a3871a0c939d2044
zephyr-bj <zephyrutd@gmail.com>
2020-02-23 10:41:43 -0800

    Update 0241_allWays2AddParentheses.cpp

--- recursion/0241_allWays2AddParentheses.cpp
+++ recursion/0241_allWays2AddParentheses.cpp
@@ -1,28 +1,19 @@
     vector<int> diffWaysToCompute(string input) {
         int n = input.size();
         vector<int>ans;
-        if(n==0)return ans;
-        int oprant = 0;
         for(int i=0; i<n; i++){
-            if(input[i]>='0'&&input[i]<='9'){
-                oprant*=10;
-                oprant+=(input[i]-'0');
-            }else{
-                vector<int>tmp1=diffWaysToCompute(input.substr(i+1));
-                vector<int>tmp2=diffWaysToCompute(input.substr(0,i));
-                for(int x=0; x<tmp1.size(); x++){
-                    for(int y=0; y<tmp2.size(); y++){
-                        if(input[i]=='+'){
-                            ans.push_back(tmp2[y]+tmp1[x]);
-                        }else if(input[i]=='-'){
-                            ans.push_back(tmp2[y]-tmp1[x]);
-                        }else if(input[i]=='*'){
-                            ans.push_back(tmp2[y]*tmp1[x]);
-                        }
+            if(input[i]=='+'||input[i]=='-'||input[i]=='*'){
+                vector<int>left=diffWaysToCompute(input.substr(0,i));
+                vector<int>right=diffWaysToCompute(input.substr(i+1));
+                for(int l=0; l<left.size(); l++){
+                    for(int r=0; r<right.size(); r++){
+                        if(input[i]=='+')ans.push_back(left[l]+right[r]);
+                        if(input[i]=='-')ans.push_back(left[l]-right[r]);
+                        if(input[i]=='*')ans.push_back(left[l]*right[r]);
                     }
                 }
             }
         }
-        if(ans.empty())ans.push_back(oprant);
+        if(ans.empty())ans.push_back(stoi(input));
         return ans;
     }
