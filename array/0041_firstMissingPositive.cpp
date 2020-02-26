e99f12c3b3e4b720901abde2c548d4d3c147c0de
zephyr-bj <zephyrutd@gmail.com>
2020-02-26 08:38:17 -0800

    Update 0041_firstMissingPositive.cpp

--- array/0041_firstMissingPositive.cpp
+++ array/0041_firstMissingPositive.cpp
@@ -1,5 +1,18 @@
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size();
+        for(int i=0; i<n; i++){
+            while(nums[i]>0&&nums[i]<n+1&&nums[i]!=nums[nums[i]-1]){
+                swap(nums[i],nums[nums[i]-1]);
+            }
+        }
+        for(int i=0; i<n; i++){
+            if(nums[i]!=i+1)return i+1;
+        }
+        return n+1;
+    }    
+// old way:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
         if(n<1)return 1;
         for(int i=0; i<n; i++){
             int k = nums[i]; // the next positive to check
