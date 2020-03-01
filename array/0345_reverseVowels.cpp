bcadeef83a5008e07ade987c467aabd72339d1ae
zephyr-bj <zephyrutd@gmail.com>
2020-03-01 10:26:56 -0800

    Update 0345_reverseVowels.cpp

--- array/0345_reverseVowels.cpp
+++ array/0345_reverseVowels.cpp
@@ -1,16 +1,14 @@
-    bool isVowel(char x) {
-        return (x=='a'||x=='e'||x=='i'||x=='o'||x=='u'||x=='A'||x=='E'||x=='I'||x=='O'||x=='U');
-    }
-    string reverseVowels(string s) {
-        int L=s.size();
-        int i=0; int j=L-1;
-        while((!isVowel(s[i]))&&i<L)i++;
+bool isVowel(char x) {
+    return (x=='a'||x=='e'||x=='i'||x=='o'||x=='u'||x=='A'||x=='E'||x=='I'||x=='O'||x=='U');
+}
+string reverseVowels(string s) {
+    int L=s.size();
+    int i=0; int j=L-1;
+    while(i<j){
+        while((!isVowel(s[i]))&&i<j)i++;
         while((!isVowel(s[j]))&&j>i)j--;
-        while(i<j){
-            swap(s[i],s[j]);
-            i++;j--;
-            while((!isVowel(s[i]))&&i<j)i++;
-            while((!isVowel(s[j]))&&j>i)j--;
-        }
-        return s;
+        swap(s[i],s[j]);
+        i++;j--;
     }
+    return s;
+}
