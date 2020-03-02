7c63c7991886300b6b5128a793aea93c02819bdd
zephyr-bj <zephyrutd@gmail.com>
2020-03-01 22:47:44 -0800

    Update 0225_stackByQueues.cpp

--- stack/0225_stackByQueues.cpp
+++ stack/0225_stackByQueues.cpp
@@ -1,70 +1,30 @@
 class MyStack {
 public:
-    queue<int>a,b,c,d;
-    void mvq(queue<int>&a, queue<int>&b, queue<int>&c){
-        if(a.size()==0)return;
-        while(a.size()!=1){
-            b.push(a.front());
-            a.pop();
-        }
-        c.push(a.front());
-        a.pop();
-        mvq(b,a,c);
-    }
     /** Initialize your data structure here. */
+    queue<int>a;
     MyStack() {
         
-    }
-    
+    }   
     /** Push element x onto stack. */
     void push(int x) {
         a.push(x);
+        for(int i=1; i<a.size(); i++){
+            a.push(a.front());
+            a.pop();
+        }
     }
-    
     /** Removes the element on top of the stack and returns that element. */
     int pop() {
-        if(c.empty()){
-            mvq(a,b,c);
-        }else{
-            if(!a.empty()){
-                while(!c.empty()){
-                    d.push(c.front());
-                    c.pop();
-                }
-                mvq(a,b,c);
-                while(!d.empty()){
-                    c.push(d.front());
-                    d.pop();
-                }
-            } 
-        }
-        int x = c.front();
-        c.pop();
-        return x;
+        int ans = a.front();
+        a.pop();
+        return ans;
     }
-    
     /** Get the top element. */
     int top() {
-        if(c.empty()){
-            mvq(a,b,c);
-        }else{
-            if(!a.empty()){
-                while(!c.empty()){
-                    d.push(c.front());
-                    c.pop();
-                }
-                mvq(a,b,c);
-                while(!d.empty()){
-                    c.push(d.front());
-                    d.pop();
-                }
-            } 
-        }
-        return c.front();
+        return a.front();
     }
-    
     /** Returns whether the stack is empty. */
     bool empty() {
-        return c.empty()&&a.empty();
+        return a.empty();
     }
 };
