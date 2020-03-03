8971f0b33c6caffb0e4acf1419762caa4af85675
zephyr-bj <zephyrutd@gmail.com>
2020-03-03 09:35:51 -0800

    Update 0147_insertionSortList.cpp

--- list/0147_insertionSortList.cpp
+++ list/0147_insertionSortList.cpp
@@ -1,23 +1,20 @@
-    ListNode* insertionSortList(ListNode* head) {
-        ListNode dummy(0);
-        dummy.next = head;
-        ListNode * frnt = head;
-        while(frnt!=NULL&&frnt->next!=NULL){
-            ListNode * back = &dummy;
-            bool insert=false;
-            while(back!=frnt){
-                if(frnt->next->val<back->next->val){
-                    ListNode * tmp = frnt->next;
-                    frnt->next=frnt->next->next;
-                    tmp->next=back->next;
-                    back->next=tmp;
-                    insert=true;
-                    break;
-                }else{
-                    back=back->next;
-                }
-            }
-            if(!insert)frnt=frnt->next;
-        }
-        return dummy.next;
+ListNode* insertionSortList(ListNode* head) {
+    ListNode dummy(0);
+    dummy.next = head;
+    ListNode * p1=head;
+    while(p1!=NULL&&p1->next!=NULL){
+        if(p1->val<=p1->next->val){
+            p1=p1->next;
+        }else{
+            ListNode * p2 = &dummy;
+            while(p2->next->val < p1->next->val){
+                p2=p2->next;
+            }//p2 -> the element should be insert after
+            ListNode * tmp=p1->next;
+            p1->next=p1->next->next;
+            tmp->next=p2->next;
+            p2->next=tmp;
+        } 
     }
+    return dummy.next;
+}
