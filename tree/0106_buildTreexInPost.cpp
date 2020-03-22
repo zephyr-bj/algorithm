b4ecb551c67750d24a72c0535fd7aff7df6f3e7b
zephyr-bj <zephyrutd@gmail.com>
2020-03-22 10:35:46 -0700

    Update 0106_buildTreexInPost.cpp

--- tree/0106_buildTreexInPost.cpp
+++ tree/0106_buildTreexInPost.cpp
@@ -13,3 +13,20 @@
         int m=postorder.size();
         return tool(inorder,0,n-1,postorder,0,m-1);
     }
+// use hash table to get rid of the while loop
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder, unordered_map<int,int>&bin,
+                        int a, int b, int c){
+        if(a>b)return NULL;
+        int x = bin[postorder[b]];
+        TreeNode * node = new TreeNode(postorder[b]);
+        node->left=buildTree(inorder, postorder, bin, a,a+(x-c)-1,c);
+        node->right=buildTree(inorder, postorder, bin, a+(x-c),b-1,x+1);
+        return node;
+    }
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        unordered_map<int,int>bin;
+        for(int i=0; i<inorder.size(); i++){
+            bin[inorder[i]]=i;
+        }
+        return buildTree(inorder,postorder,bin,0,postorder.size()-1,0);
+    }
