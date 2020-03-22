9671324f7fbac22d877b309b5e9babd35ddccc1b
zephyr-bj <zephyrutd@gmail.com>
2020-03-21 23:52:44 -0700

    Update 0095_uniqueBSTII.cpp

--- tree/0095_uniqueBSTII.cpp
+++ tree/0095_uniqueBSTII.cpp
@@ -9,6 +9,23 @@
  */
 
 //recursive solution
+/*
+# of trees:
+
+n = 0: 1
+n = 1: 1
+n = 2: 2
+n = 3: 5
+n = 4: 14
+n = 5: 42
+n = 6: 132
+…
+Trees(n) = Trees(0)*Trees(n-1) + Trees(1)*Trees(n-2) + … + Tress(n-1)*Trees(0)
+
+Time complexity: O(3^n)
+
+Space complexity: O(3^n)
+*/
 class Solution {
 public:
     vector<TreeNode*> treeTool(int a, int b){
@@ -79,3 +96,36 @@ public:
         }
         return bin;
     }
+};
+
+// another DP solution
+class Solution {
+public:
+    vector<TreeNode*> generateTrees(int n) {
+        if(n<1)return vector<TreeNode*>();
+        vector<vector<TreeNode*>>bin(n+1);
+        bin[0]=vector<TreeNode*>(1,NULL);
+        for(int i=1; i<=n; i++){
+            vector<TreeNode*>trees;
+            for(int j=1; j<=i; j++){
+                for(auto x:bin[j-1]){
+                    for(auto y:bin[i-j]){
+                        TreeNode*root = new TreeNode(j);
+                        root->left=x;
+                        root->right=clone(y,j);
+                        trees.push_back(root);
+                    }
+                }
+            }
+            bin[i]=trees;
+        }
+        return bin[n];
+    }
+    TreeNode*clone(TreeNode*root, int shift){
+        if(root==NULL)return root;
+        TreeNode*ans=new TreeNode(root->val+shift);
+        ans->left=clone(root->left,shift);
+        ans->right=clone(root->right,shift);
+        return ans;
+    }
+};
