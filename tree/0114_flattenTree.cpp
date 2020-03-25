3c226f692a30fe32c2f35cbe07c68bbe1299d704
zephyr-bj <zephyrutd@gmail.com>
2020-03-25 09:46:09 -0700

    Update 0114_flattenTree.cpp

--- tree/0114_flattenTree.cpp
+++ tree/0114_flattenTree.cpp
@@ -12,20 +12,30 @@
         cur = cur->right;
       }
     }
-// 0ms recursive solution
+// recursive solution:
+// 1: traversal order: right->left->root
+// 2: "pre" keeps the last visited node
     void flatten(TreeNode* root) {
         flatten(root, NULL);
     }
     TreeNode* flatten(TreeNode* root, TreeNode* pre) {
         if (root == NULL) return pre;
-        // flatten the right subtree
         pre = flatten(root->right, pre);
-        // flatten the left subtree, and assign the right subtree as right
         pre = flatten(root->left, pre);
-        // remove left
         root->left = NULL;
-        // assign the flatten left subtree as right
         root->right = pre;
-        // return self as a flattened tree 
         return root;
+    }
+// equivalent to the following
+    void flatten(TreeNode* root) {
+        TreeNode * pre = NULL;
+        flatten(root,&pre);
+    }
+    void flatten(TreeNode* root, TreeNode** pre){
+        if(root==NULL)return;
+        flatten(root->right, pre);
+        flatten(root->left,pre);
+        root->left=NULL;
+        root->right=*pre;
+        *pre = root;
     }
