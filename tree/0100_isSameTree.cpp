4f0939bd1580d01ca8fa0d1b39560368f1cfd824
zephyr-bj <zephyrutd@gmail.com>
2020-04-12 10:02:23 -0700

    Update 0100_isSameTree.cpp

--- tree/0100_isSameTree.cpp
+++ tree/0100_isSameTree.cpp
@@ -1,11 +1,10 @@
-bool isSameTree(TreeNode p, TreeNode* q) {
-    if (p && !q) return false;
-    if (!p && q) return false;
-    if (!p && !q) return true;
-    if (isSameTree(p->left, q->left) && isSameTree(p->right, q->right) && p->val == q->val)
-        return true;
-    else
-        return false;
+bool isSameTree(TreeNode* p, TreeNode* q) {
+    if(p==NULL&&q==NULL)return true;
+    if(p==NULL||q==NULL)return false;
+    if(p->val!=q->val)return false;
+    if(!isSameTree(p->left,q->left))return false;
+    if(!isSameTree(p->right,q->right))return false;
+    return true;
 }
 /*
 iterative works fine too
