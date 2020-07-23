6c403fbc720915496a323707311096945b07f74b
zephyr-bj <zephyrutd@gmail.com>
2020-07-23 05:54:51 -0700

    Update 0126_wordLadderII.cpp

--- directed_graph/0126_wordLadderII.cpp
+++ directed_graph/0126_wordLadderII.cpp
@@ -1,3 +1,70 @@
+    
+// two end BFS, 10 time faster, 80% less memory than the standard BST. 
+    bool bst(unordered_set<string>words1, 
+             unordered_set<string>words2, 
+             unordered_set<string>&dict, 
+             unordered_map<string, vector<string>>&connections, bool reverse){
+        if(words1.size()==0)return false;
+        if(words1.size()>words2.size())
+            return bst(words2, words1, dict, connections, !reverse);
+        unordered_set<string>words3;
+        bool done = false;
+        for(auto w:words1)dict.erase(w);
+        for(auto w:words2)dict.erase(w);
+        for(auto s:words1){
+            for(int i=0; i<s.size(); i++){
+                string ss = s;
+                for(int c='a'; c<='z'; c++){
+                    if(c!=s[i]){
+                        ss[i]=c;
+                        if(words2.find(ss)!=words2.end()){
+                            done = true;
+                            if(reverse)connections[ss].push_back(s);
+                            else connections[s].push_back(ss);
+                        }else if(dict.find(ss)!=dict.end()){
+                            words3.insert(ss);
+                            if(reverse)connections[ss].push_back(s);
+                            else connections[s].push_back(ss);
+                        }
+                    }
+                }
+            }
+        }
+        if(done) return true;
+        else return bst(words3, words2, dict, connections, reverse);
+    }
+    void getpath(string beginWord, string endWord, 
+                 unordered_map<string, vector<string>>&connections,
+                 vector<string>&path, vector<vector<string>>&paths){
+        if(beginWord == endWord){
+            paths.push_back(path);
+        }else{
+            for(auto w:connections[beginWord]){
+                path.push_back(w);
+                getpath(w,endWord,connections,path,paths);
+                path.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string>dict(wordList.begin(), wordList.end());
+        if(dict.find(endWord)==dict.end())return vector<vector<string>>();
+        dict.erase(endWord);
+        vector<vector<string>>paths;
+        vector<string>path({beginWord});
+        if(beginWord==endWord) {
+            paths.push_back(path);
+            return paths;
+        }
+        unordered_set<string>words1({beginWord});
+        unordered_set<string>words2({endWord});
+        unordered_map<string,vector<string>>connections;
+        if(bst(words1,words2,dict,connections,false)){
+            getpath(beginWord,endWord,connections,path,paths);
+        }
+        return paths;
+    }
+
     //It can be solved with standard BFS. The tricky idea is doing BFS of paths instead of words!
     //Then the queue becomes a queue of paths.
     
