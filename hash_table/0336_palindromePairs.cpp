170fca3d3b5d9e7a6d0d06ab9213bda23d4791a6
zephyr-bj <zephyrutd@gmail.com>
2020-09-11 17:29:33 -0700

    Update 0336_palindromePairs.cpp

--- hash_table/0336_palindromePairs.cpp
+++ hash_table/0336_palindromePairs.cpp
@@ -1,48 +1,36 @@
     // one to all relationships, create map at the very beginning
     // I used to try to add possible sub strings to table, but it may not be a good idea, I have not seen a table with larger size than the input data
-    // the "j>-1" prevent duplicate
+    // use "i>0" at one branch to prevent duplicate
     
-    bool isPalindrome(string &s, int i, int j){
+   bool isPalindrome(string s){
+        int i=0; int j=s.size()-1;
         while(i<j){
-            if(s[i]!=s[j])return false;
-            i++; j--;
+            if(s[i++]!=s[j--])return false;
         }
         return true;
     }
-    string reverseWord(string &s){
-        int x = 0; int y=s.size()-1;
-        string ans=s;
-        while(x<y){
-            swap(ans[x],ans[y]);
-            x++; y--;
-        }
-        return ans;
-    }
     vector<vector<int>> palindromePairs(vector<string>& words) {
-        vector<vector<int>>ans;
-        int n = words.size();
         unordered_map<string,int>bin;
-        for(int i=0; i<n; i++){
-            bin[words[i]]=i;
-        }
-        for(int i=0; i<n; i++){
-            string rword = reverseWord(words[i]);
-            int m = words[i].size();
-            for(int j=-1; j<m; j++){
-                if(isPalindrome(words[i],0,j)){
-                    string s1 = rword.substr(0,m-j-1);
-                    if(bin.find(s1)!=bin.end()&&bin[s1]!=i){
-                        vector<int>pair(2,i);
-                        pair[0]=bin[s1];
-                        ans.push_back(pair);
-                    } 
+        int sz = words.size();
+        for(int i=0; i<sz; i++)bin[words[i]]=i;
+        vector<vector<int>>ans;
+        for(int k=0; k<sz; k++){
+            int n = words[k].size();  
+            string rword = words[k];
+            reverse(rword.begin(), rword.end());
+            for(int i=0; i<=n; i++){
+                string s1 = words[k].substr(0,i);
+                if(isPalindrome(s1)){
+                    string s2 = rword.substr(0,n-i);
+                    if(bin.find(s2)!=bin.end()&&bin[s2]!=k){
+                        ans.push_back(vector<int>({bin[s2],bin[words[k]]}));
+                    }
                 }
-                if(isPalindrome(words[i],m-j-1,m-1)){
-                    string s2 = rword.substr(j>=m-1&&m>0?m-1:j+1,m-j-1);
-                    if(j>-1&&bin.find(s2)!=bin.end()&&bin[s2]!=i){
-                        vector<int>pair(2,i);
-                        pair[1]=bin[s2];
-                        ans.push_back(pair);
+                string s3 = words[k].substr(n-i,i);
+                if(i>0&&isPalindrome(s3)){
+                    string s4 = rword.substr(i);
+                    if(bin.find(s4)!=bin.end()&&bin[s4]!=k){
+                        ans.push_back(vector<int>({bin[words[k]],bin[s4]}));
                     }
                 }
             }
