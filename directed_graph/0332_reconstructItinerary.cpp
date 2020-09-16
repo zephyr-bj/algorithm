43c983b72a0908eccadc10fbebe49d46319d60d5
zephyr-bj <zephyrutd@gmail.com>
2020-09-16 10:03:42 -0700

    Update 0332_reconstructItinerary.cpp

--- directed_graph/0332_reconstructItinerary.cpp
+++ directed_graph/0332_reconstructItinerary.cpp
@@ -1,35 +1,22 @@
-/*DFS*/    
-bool tool(int n, string port, vector<string>&ans, unordered_map<string,multimap<string,int>>&bin){
-        if(n==0)return true;
-        unordered_map<string,multimap<string,int>>::iterator it = bin.find(port);
-        if(it==bin.end())return false;
-        for(multimap<string,int>::iterator j=it->second.begin(); j!=it->second.end(); j++){
-            if(j->second==1)continue;
-            string tmp = j->first;
-            ans.push_back(tmp);
-            j->second=1;
-            if(tool(n-1,tmp, ans, bin))return true;
-            else{
-                j->second=0;
-                ans.pop_back();
+/*DFS*/    //nothing but a traverse
+    bool dfs(unordered_map<string, map<string,int>>&G, string start, int stops, vector<string>&path){
+        if(path.size()==stops+1)return true;
+        for(auto m: G[start])
+            if(m.second>0){
+                path.push_back(m.first);
+                G[start][m.first]--;
+                if(dfs(G,m.first,stops,path))return true;
+                G[start][m.first]++;
+                path.pop_back();
             }
-        }
         return false;
     }
-    vector<string> findItinerary(vector<pair<string, string>> tickets) {
-        int n = tickets.size();
-        unordered_map<string,multimap<string,int>>bin;
-        for(int i=0; i<n; i++){
-            if(bin.find(tickets[i].first)==bin.end()){
-                multimap<string,int>tmp_set;
-                tmp_set.insert(pair<string,int>(tickets[i].second,0));
-                bin[tickets[i].first]=tmp_set;
-            }else{
-                bin[tickets[i].first].insert(pair<string,int>(tickets[i].second,0));
-            }
-        }
-        string port("JFK");
-        vector<string>ans(1,port);
-        tool(n, port, ans, bin);
-        return ans;
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        unordered_map<string,map<string,int>>G;
+        for(auto t:tickets)
+            G[t[0]][t[1]]+=1;
+        int K = tickets.size();
+        vector<string>path(1,"JFK");
+        dfs(G,"JFK", K, path);
+        return path;
     }
