693b046c13d6bdbc18830cd8866d330802dffb2c
zephyr-bj <zephyrutd@gmail.com>
2020-09-16 16:52:11 -0700

    Update 0211_trieRE.cpp

--- trie/0211_trieRE.cpp
+++ trie/0211_trieRE.cpp
@@ -1,50 +1,83 @@
+/* Trie method */ //faster hash table method followed
+struct TrieNode{
+    bool isEnd;
+    TrieNode* next[26];
+    TrieNode():isEnd(false){
+        memset(next, NULL, sizeof(next));
+    }
+};
+
 class WordDictionary {
 public:
-    struct TrieNode{
-        vector<TrieNode*>br;
-        bool isEnd;
-        TrieNode(){
-            isEnd=false;
-            for(int i=0; i<26; i++)br.push_back(NULL);
-        }
-    };
-    /** Initialize your data structure here. */
-    TrieNode * root;
     WordDictionary() {
-        root=new TrieNode;   
+        root = new TrieNode();
+    }
+    
+    void addWord(string word) {
+        TrieNode* node = root;
+        for(auto c: word){
+            if(!node->next[c - 'a']) node->next[c - 'a'] = new TrieNode();
+            node = node->next[c - 'a'];
+        }
+        node->isEnd = true;
+    }
+    
+    bool search(string word) {
+        return searchWord(word, root);
+    }
+
+private:
+    TrieNode* root;
+    
+    bool searchWord(string word, TrieNode* node){
+        for(int i = 0; i < word.size(); i++){
+            char c = word[i];
+            if(c != '.'){
+                if(!node->next[c - 'a']) return false;
+                node = node->next[c - 'a'];
+            }
+            else{
+                for(int j=0; j < 26; j++){
+                    if(node->next[j]) 
+                        if(searchWord(word.substr(i + 1), node->next[j]))return true;
+                }
+                return false;
+            }
+        }
+        return node->isEnd;
+    }
+};
+
+
+
+/* hash table method */
+class WordDictionary {
+public:
+    /** Initialize your data structure here. */
+    unordered_map<int,vector<string>>bin;
+    WordDictionary() {
+        
     }
     
     /** Adds a word into the data structure. */
     void addWord(string word) {
-        int n = word.size();
-        TrieNode * tmp=root;
-        for(int i=0; i<n; i++){
-            if(tmp->br[word[i]-'a']==NULL){
-                tmp->br[word[i]-'a']=new TrieNode;
-            }
-            tmp=tmp->br[word[i]-'a'];
-        }
-        tmp->isEnd=true;
+        bin[word.size()].push_back(word);
     }
     
     /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
-    bool searchtool(TrieNode * p, string &word, int x, int n){
-        if(word[x]=='.'){
-            for(int j=0; j<26; j++){
-                if(p->br[j]!=NULL){
-                    if(x==n-1&&p->br[j]->isEnd)return true;
-                    if(searchtool(p->br[j], word, x+1, n))return true;
-                }
-            }
-            return false;
-        }else if(word[x]>='a'&&word[x]<='z'&&p->br[word[x]-'a']!=NULL){
-            if(x==n-1&&p->br[word[x]-'a']->isEnd)return true;
-            return searchtool(p->br[word[x]-'a'], word, x+1, n);
-        }
-        else return false;
-    }
     bool search(string word) {
-        int n = word.size();
-        return searchtool(root, word, 0, n);
+        for(auto x:bin[word.size()]){
+            if(isEqual(word,x))return true;
+        }
+        return false;
+    }
+private:
+    bool isEqual(string s, string t){
+        int n = s.size();
+        for(int i=0; i<s.size(); i++){
+            if(s[i]=='.')continue;
+            else if(s[i]!=t[i])return false;
+        }
+        return true;
     }
 };
