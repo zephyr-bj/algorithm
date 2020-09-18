2450d842a5d67bec00695e839144153b164149fe
zephyr-bj <zephyrutd@gmail.com>
2020-09-17 17:59:24 -0700

    Update 0085_maximalRectangle.cpp

--- dp/0085_maximalRectangle.cpp
+++ dp/0085_maximalRectangle.cpp
@@ -1,3 +1,32 @@
+    // O(nm)
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int n = matrix.size();
+        if(n<1)return 0;
+        int m = matrix[0].size();
+        vector<int>left(m,0);
+        vector<int>right(m,m-1);
+        vector<int>height(m,0);
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j]=='1')height[j]+=1;
+                else height[j]=0;
+            }
+            int cur_left = 0;
+            for(int j=0; j<m; j++){
+                if(matrix[i][j]=='1')left[j]=max(left[j],cur_left);
+                else {left[j]=0; cur_left=j+1;}
+            }
+            int cur_right = m-1;
+            for(int j = m-1; j>=0; j--){
+                if(matrix[i][j]=='1')right[j]=min(right[j],cur_right);
+                else {right[j]=m-1; cur_right=j-1;}
+            }
+            for(int j=0; j<m; j++)
+                ans = max(ans,(right[j]-left[j]+1)*height[j]);
+        }
+        return ans;
+    }
     //O(nmm);
     int maximalRectangle(vector<vector<char>>& matrix) {
         int n=matrix.size();
