8d8f22a80ae271e1fe736da0ee34dd8db3928fad
zephyr-bj <zephyrutd@gmail.com>
2020-09-18 23:01:31 -0700

    Update 0174_dungeonGame.cpp

--- dp/0174_dungeonGame.cpp
+++ dp/0174_dungeonGame.cpp
@@ -1,28 +1,22 @@
     int calculateMinimumHP(vector<vector<int>>& dungeon) {
         int n = dungeon.size();
+        if(n<1)return 0;
         int m = dungeon[0].size();
-        vector<int>row(m,0);
-        vector<vector<int>>bin(n,row);
-        for(int i=n-1; i>=0; i--){
-            for(int j=m-1; j>=0; j--){
-                if(i==n-1&&j==m-1){
-                    if(dungeon[n-1][m-1]>=0)bin[n-1][m-1]=1;
-                    else bin[n-1][m-1]=1-dungeon[n-1][m-1];
-                }else{
-                    int x=INT_MAX;
-                    if(i<n-1&&bin[i+1][j]<x)x=bin[i+1][j];
-                    if(j<m-1&&bin[i][j+1]<x)x=bin[i][j+1];
-                    if(dungeon[i][j]<=0){
-                        bin[i][j]=x-dungeon[i][j];
-                    }else{
-                        if(dungeon[i][j]<x)bin[i][j]=x-dungeon[i][j];
-                        else bin[i][j]=1;
-                    }
-                }
+        vector<vector<int>>hp(n,vector<int>(m,0));
+        hp[n-1][m-1]=dungeon[n-1][m-1]>=0? 1: 1-dungeon[n-1][m-1];
+        for(int i=n-2; i>=0; i--){//x+dungeon[i][m-1]=hp[i+1][m-1];
+            hp[i][m-1]=hp[i+1][m-1]-dungeon[i][m-1]<=0 ? 1 : hp[i+1][m-1]-dungeon[i][m-1];
+        }
+        for(int j=m-2; j>=0; j--){
+            hp[n-1][j]=hp[n-1][j+1]-dungeon[n-1][j]<=0 ? 1 : hp[n-1][j+1]-dungeon[n-1][j];
+        }
+        for(int i=n-2; i>=0; i--){
+            for(int j=m-2; j>=0; j--){
+                hp[i][j]=min(hp[i+1][j],hp[i][j+1])-dungeon[i][j]<=0 ? 1: min(hp[i+1][j],hp[i][j+1])-dungeon[i][j];
             }
         }
-        return bin[0][0];
-    }  
+        return hp[0][0];
+    }
     /*
     direction matters, from target point to start point is much easier than from start point to target point
     */
