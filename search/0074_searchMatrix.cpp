1dd4d1807ae4ff183c2c20d97cbf301ada4edd10
zephyr-bj <zephyrutd@gmail.com>
2020-09-22 07:17:45 -0700

    Update 0074_searchMatrix.cpp

--- search/0074_searchMatrix.cpp
+++ search/0074_searchMatrix.cpp
@@ -13,3 +13,18 @@
         }
         return false;
     }
+
+//another solution
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        if(n<1)return false;
+        int m = matrix[0].size();
+        int i= n-1; 
+        int j=0;
+        while(i>=0 && j<m){
+            if(matrix[i][j]==target)return true;
+            else if(matrix[i][j]<target)j++;
+            else i--;
+        }
+        return false;
+    }
