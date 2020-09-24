7926c8aac08a5e91432f2b6ba7746f15a877f2ed
zephyr-bj <zephyrutd@gmail.com>
2020-09-24 09:22:03 -0700

    Update 0378_kthSmallestSortedMatrix.cpp

--- search/0378_kthSmallestSortedMatrix.cpp
+++ search/0378_kthSmallestSortedMatrix.cpp
@@ -1,19 +1,14 @@
-    int kthSmallest(vector<vector<int>>& a, int k) {
-        int l,h,m,ans,n;
-        n=a.size();
-        l=a[0][0];
-        h=a[n-1][n-1];
-        while(l<=h){
-            m=l+(h-l)/2;
-            int c=0;
-            for(int i=0;i<n;i++)
-                c+=upper_bound(begin(a[i]),end(a[i]),m)-begin(a[i]);
-            if(c<k)
-                l=m+1;
-            else{
-                ans=m;
-                h=m-1;
-            }
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n = matrix.size();
+        int x = matrix[0][0];
+        int y = matrix[n-1][n-1];
+        while(x<y){
+            int m = (x+y)/2;
+            int cnt = 0;
+            for(int i=0; i<n; i++)
+                cnt += upper_bound(matrix[i].begin(), matrix[i].end(), m)-matrix[i].begin();
+            if(cnt<k)x=m+1;
+            else y=m;
         }
-        return ans;
+        return x;
     }
