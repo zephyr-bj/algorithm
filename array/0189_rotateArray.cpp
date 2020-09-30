52580500c4f01a5772153799f2423cf8b01b35f9
zephyr-bj <zephyrutd@gmail.com>
2020-09-29 22:30:39 -0700

    Update 0189_rotateArray.cpp

--- array/0189_rotateArray.cpp
+++ array/0189_rotateArray.cpp
@@ -5,3 +5,16 @@
     reverse(nums.begin() + n - k,nums.end());
     reverse(nums.begin(),nums.end());
  }
+/* or 
+    void rotate(vector<int>& nums, int k) {
+        if(k==0)return;
+        int n = nums.size();
+        k=k%n;
+        int a = 0; int b = n-k-1;
+        while(a<b)swap(nums[a++],nums[b--]);
+        a=n-k; b=n-1;
+        while(a<b)swap(nums[a++],nums[b--]);
+        a=0; b=n-1;
+        while(a<b)swap(nums[a++],nums[b--]);
+    }
+*/
