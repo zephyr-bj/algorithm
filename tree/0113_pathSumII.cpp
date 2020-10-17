281e0d59c75ea63985e50b7dc28aed260e2399cb
zephyr-bj <zephyrutd@gmail.com>
2020-10-16 21:53:33 -0700

    Update 0113_pathSumII.cpp

--- tree/0113_pathSumII.cpp
+++ tree/0113_pathSumII.cpp
@@ -1,20 +1,16 @@
-    void tool(vector<vector<int>>&ans, vector<int>&path, TreeNode* root, int sum){
-        if(root==NULL){
-            return;
-        }else{
-            path.push_back(root->val);
-            if(root->left!=NULL)tool(ans,path,root->left,sum-root->val);
-            if(root->right!=NULL)tool(ans,path,root->right,sum-root->val);
-            if(root->left==NULL&&root->right==NULL){
-                if(root->val==sum)ans.push_back(path);
-            }
-            path.pop_back();
+    void pathSum(TreeNode* root, int sum, vector<vector<int>>&ans, vector<int>&path){
+        if(root==NULL)return;
+        path.push_back(root->val);
+        if(root->left==NULL&&root->right==NULL){
+            if(root->val==sum)ans.push_back(path);
         }
+        pathSum(root->left,sum-root->val,ans,path);
+        pathSum(root->right,sum-root->val,ans,path);
+        path.pop_back();
     }
     vector<vector<int>> pathSum(TreeNode* root, int sum) {
         vector<vector<int>>ans;
-        if(root==NULL)return ans;
         vector<int>path;
-        tool(ans,path,root,sum);
+        pathSum(root,sum,ans,path);
         return ans;
     }
