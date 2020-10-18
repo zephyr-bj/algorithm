94242b6cd97ee37c53bd4feda91b4db0e6eaecbb
zephyr-bj <zephyrutd@gmail.com>
2020-10-18 00:03:31 -0700

    Create array_modify_11.cpp

---
+++ summary/array_modify_11.cpp
@@ -0,0 +1,203 @@
+/* sort array [4] bucket sort (0164) quick sort (0215) wiggle sort II (0324) largest number string (0179)
+                  partition (*) 3-way partition (*)
+ * reverse array[4] reverse words in sentence (0151) reverse string (0344) reverse vowels (0345)
+ * retate array [1] (0189)
+ * merge array [1] (0088)
+ * shuffle array [1] (0384)
+ */
+//maximum gap (0164)
+    int maximumGap(vector<int>& nums) {
+        int n = nums.size();
+        if(n<2)return 0;
+        int lb = nums[0];
+        int ub = nums[0];
+        for(int i=1; i<n; i++){
+            if(nums[i]>ub)ub=nums[i];
+            if(nums[i]<lb)lb=nums[i];
+        }
+        if(lb==ub)return 0;
+        int wide = max((ub-lb)/(n-1),1);
+        int sz = (ub-lb)/wide+1;
+        vector<int>lbin(sz,INT_MAX);
+        vector<int>ubin(sz,INT_MIN);
+        for(int i=0; i<n; i++){
+            int idx = (nums[i]-lb)/wide;
+            if(nums[i]<lbin[idx])lbin[idx]=nums[i];
+            if(nums[i]>ubin[idx])ubin[idx]=nums[i];
+        }
+        int ans =0;
+        int j=0;
+        for(int i=1; i<sz; i++){
+            if(lbin[i]==INT_MAX||ubin[i]==INT_MIN)continue;
+            ans = max(ans,lbin[i]-ubin[j]);
+            j=i;
+        }
+        return ans;
+    }
+//largest number string by non negative integers (0179) trick for compare rule
+    static bool my_cmp(int a, int b){
+        return to_string(a)+to_string(b) > to_string(b)+to_string(a);
+    }
+    string largestNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), my_cmp);
+        if(nums.empty()||nums[0]==0)return string("0");
+        string ans;
+        for(auto x:nums)ans+=to_string(x);
+        return ans;
+    }
+//Kth largest element (0215)
+    // we need the partition function
+    int findKthLargest(vector<int>& nums, int k) {
+        int left = 0, right = nums.size() - 1;
+        while (true) {
+            int idx = partition(nums, left, right);
+            if (idx == k - 1) {
+                kth = nums[idx];
+                break;
+            }
+            if (idx < k - 1) {
+                left = idx + 1;
+            } else {
+                right = idx - 1;
+            }
+        }
+        return kth;
+    }
+//wiggle sort II (0324)
+void wiggleSort(vector<int>& nums) {
+    # define A(i) nums[((1+i*2)%(n|1))]
+    auto midptr = nums.begin() + n / 2;
+    nth_element(nums.begin(), midptr, nums.end());
+    int pivot = *midptr;
+    int i=0; int j=0; int k=n-1;
+    while(j<=k){
+        if(A(j)<pivot){
+            swap(A(j),A(k--));
+        }else if(A(j)>pivot){
+            swap(A(j++),A(i++));
+        }else{
+            j++;
+        }
+    }
+}
+// partition
+int partition(vector<int>&arr, int l, int r) { 
+    int x = arr[r], i = l; 
+    for (int j = l; j <= r - 1; j++) { 
+        if (arr[j] <= x) { 
+            swap(arr[i], arr[j]); 
+            i++; 
+        } 
+    } 
+    swap(arr[i], arr[r]); 
+    return i; 
+} 
+//3-way partition
+void partition3Way(vector<int>&arr, int l, int r, int& x, int&y) { 
+    int pivot = arr[r];
+    int i = l; int j=l; int k=r-1;
+    while(j<=k){
+        if(arr[j]<pivot){
+            swap(arr[i],arr[j]);
+            i++; j++;
+        }else if(arr[j]>pivot){
+            swap(arr[j],arr[k]);
+            k--;
+        }else{
+            j++; 
+        } 
+    } 
+    swap(arr[j],arr[r]);
+    x=i;y=j;
+}
+//reverse every word in a string (0151)
+    string reverseWords(string s) {
+        int n = s.size();
+        int i=0; 
+        int l = n; 
+        for(int j=0; j<n; j++){
+            s[i]=s[j];
+            if(s[j]==' '&&(j==0||s[j-1]==' '))l--;
+            else i++;
+        }
+        if(s[l-1]==' ')l--;
+        s.erase(l);
+        
+        int x=0; int y=l-1;
+        while(x<y){
+            swap(s[x],s[y]);
+            x++;y--;
+        }
+        
+        i=0;
+        for(int j=0; j<=l; j++){
+            if(s[j]==' '||j==l){
+                int x = i; int y=j-1;
+                while(x<y){
+                    swap(s[x],s[y]);
+                    x++;y--;
+                }
+                i=j+1;
+            }
+        }
+        return s;
+    }
+//reverse string (0344)
+    void reverseString(vector<char>& s) {
+        int L = s.size();
+        int i=0; int j=L-1;
+        while(i<j){
+            swap(s[i],s[j]);
+            i++; j--;
+        }
+    }
+//reverse vowels (0345)
+bool isVowel(char x) {
+    return (x=='a'||x=='e'||x=='i'||x=='o'||x=='u'||x=='A'||x=='E'||x=='I'||x=='O'||x=='U');
+}
+string reverseVowels(string s) {
+    int L=s.size();
+    int i=0; int j=L-1;
+    while(i<j){
+        while((!isVowel(s[i]))&&i<j)i++;
+        while((!isVowel(s[j]))&&j>i)j--;
+        swap(s[i],s[j]);
+        i++;j--;
+    }
+    return s;
+}
+//retate array by k (0189), consider the case in which n%k==0
+ void rotate(vector<int>& nums, int k) {
+    int n = nums.size();
+    k = k % n;
+    reverse(nums.begin(),nums.begin() + n - k);
+    reverse(nums.begin() + n - k,nums.end());
+    reverse(nums.begin(),nums.end());
+ }
+//merge two arrays (0088)
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int k=m+n-1;
+        int j=m-1;
+        int i=n-1;
+        while(i>=0||j>=0){
+            if(j<0){
+                nums1[k]=nums2[i];i--;
+            }else if(i<0){
+                nums1[k]=nums1[j];j--;
+            }else if(nums1[j]>nums2[i]){
+                nums1[k]=nums1[j];j--;
+            }else{
+                nums1[k]=nums2[i];i--;
+            }
+            k--;
+        }
+    }  
+//shuffle array (0384)
+    vector<int> shuffle() {
+        vector<int>res=setx;
+        for(int i=0; i<n; i++){
+            int x = rand()%(n-i);
+            swap(res[n-i-1],res[x]);
+        }
+        return res;
+    }
