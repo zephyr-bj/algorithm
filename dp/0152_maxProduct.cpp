228d13c5603b427a913f57dba8ea33fe3470859b
zephyr-bj <zephyrutd@gmail.com>
2020-11-01 10:25:17 -0800

    Update 0152_maxProduct.cpp

--- dp/0152_maxProduct.cpp
+++ dp/0152_maxProduct.cpp
@@ -2,22 +2,15 @@
     int maxProduct(vector<int>& nums) {
         int n = nums.size();
         if(n<1)return 0;
-        int minP = nums[0];
-        int maxP = nums[0];
+        int a = nums[0];
+        int b = nums[0];
         int ans = nums[0];
-        for(int i=1; i<n; i++){
-            if(nums[i]>0){
-                minP=min(nums[i],minP*nums[i]);
-                maxP=max(nums[i],maxP*nums[i]);
-            }else if(nums[i]<0){
-                // this temperary variable is important
-                int minCP = minP; 
-                minP=min(nums[i],maxP*nums[i]);
-                maxP=max(nums[i],minCP*nums[i]);
-            }else{
-                minP=0; maxP=0;
-            }
-            ans = max(ans,maxP);
+        for(int i=1;i<n;i++){
+            int x = min(nums[i],min(a*nums[i],b*nums[i]));
+            int y = max(nums[i],max(a*nums[i],b*nums[i]));
+            a = x;
+            b = y;
+            ans = max(ans,b);
         }
         return ans;
     }
