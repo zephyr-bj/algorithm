e0cec21bbcbb563fe07ce46a05bec33ac95c5635
zephyr-bj <zephyrutd@gmail.com>
2020-11-01 14:37:47 -0800

    Update 0010_regularExpressionMatch.cpp

--- dp/0010_regularExpressionMatch.cpp
+++ dp/0010_regularExpressionMatch.cpp
@@ -1,30 +1,21 @@
     bool isMatch(string s, string p) {
-        int n = s.size();
-        int m = p.size();
-        vector<int>tmp(n+1,0);
-        vector<vector<int>>table(m+1,tmp);
-        table[0][0]=1;
-        for(int i=1; i<m+1; i++){
-            if(p[i-1]!='*')table[i][0]=0;
-            else if(table[i-2][0]==1)table[i][0]=1;
+        int n = p.size(); 
+        int m = s.size();
+        vector<vector<bool>>re(n+1,vector<bool>(m+1,false));
+        re[0][0]=1;
+        for(int i=1; i<n; i++){
+            if(p[i]=='*'&&p[i-1]!='*'&&re[i-1][0])re[i+1][0]=true;
         }
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(p[i]=='*'){
-                // 'x*' matches zero 'x' in s 
-                    if(i>1&&table[i-1][j+1]==1)table[i+1][j+1]=1;
-                // 'x*' matches one 'x' in s
-                    if((p[i-1]==s[j]||p[i-1]=='.')&&table[i][j+1]==1)table[i+1][j+1]=1;
-                // 'x*' matches two 'x's in s
-                    if((p[i-1]==s[j]||p[i-1]=='.')&&table[i][j]==1)table[i+1][j+1]=1;
-                // 'x*' matches more than two 'x's in s
-                    if((p[i-1]==s[j]||p[i-1]=='.')&&table[i+1][j]==1)table[i+1][j+1]=1;
-                }
-                // 'p[i] matches s[j] or p[i] matches anything'
-                if(p[i]==s[j]||p[i]=='.'){
-                    if(table[i][j]==1)table[i+1][j+1]=1;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(p[i]=='.'||p[i]==s[j]){
+                    if(re[i][j])re[i+1][j+1]=true;
+                }else if(i>0&&p[i]=='*'){
+                    if(re[i-1][j+1])re[i+1][j+1]=true;
+                    if(re[i-1][j]&&(p[i-1]==s[j]||p[i-1]=='.'))re[i+1][j+1]=true;
+                    if(re[i+1][j]&&(p[i-1]==s[j]||p[i-1]=='.'))re[i+1][j+1]=true;
                 }
             }
         }
-        return table[m][n];
+        return re[n][m];
     }
