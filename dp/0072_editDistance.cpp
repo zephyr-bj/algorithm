8811119dc849e100c2c2882de0c2967f36489a6b
zephyr-bj <zephyrutd@gmail.com>
2020-11-01 14:44:12 -0800

    Update 0072_editDistance.cpp

--- dp/0072_editDistance.cpp
+++ dp/0072_editDistance.cpp
@@ -1,20 +1,19 @@
     int minDistance(string word1, string word2) {
-        int n=word1.size();
-        int m=word2.size();
-        vector<vector<int>>bin(n+1,vector<int>(m+1,0));
+        int m = word2.size();
+        int n = word1.size();
+        vector<vector<int>>ed(n+1,vector<int>(m+1,0));
+        for(int i=0; i<n; i++)ed[i+1][0]=i+1;
+        for(int j=0; j<m; j++)ed[0][j+1]=j+1;
         for(int i=0; i<n; i++){
-            bin[i+1][0]=i+1;
-        }
-        for(int j=0; j<m; j++){
-            bin[0][j+1]=j+1;
-        }
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
-                int replace=word1[i-1]==word2[j-1]?0:1;
-                bin[i][j]=min(replace+bin[i-1][j-1],min(bin[i-1][j]+1,bin[i][j-1]+1));
+            for(int j=0; j<m; j++){
+                if(word1[i]==word2[j]){
+                    ed[i+1][j+1]=ed[i][j];
+                }else{
+                    ed[i+1][j+1]=min(ed[i][j],min(ed[i+1][j],ed[i][j+1]))+1;
+                }
             }
         }
-        return bin[n][m];
+        return ed[n][m];
     }
     /*
     single variable
