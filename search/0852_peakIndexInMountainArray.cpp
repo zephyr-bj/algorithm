150a1a5b23e21d4cdf21c5f110ccdfd220e5c5d7
zephyr-bj <zephyrutd@gmail.com>
2020-11-05 22:45:48 -0800

    Update 0852_peakIndexInMountainArray.cpp

--- search/0852_peakIndexInMountainArray.cpp
+++ search/0852_peakIndexInMountainArray.cpp
@@ -1,10 +1,9 @@
-    int peakFindUtil(vector<int>&arr, int x, int y, int n){
-        int mid = (x+y)/2;
-        if(mid>0&&arr[mid]>arr[mid-1]&&mid<n-1&&arr[mid]>arr[mid+1])return mid;
-        else if(mid>0&&arr[mid]<arr[mid-1])return peakFindUtil(arr, x, mid-1, n);
-        else return peakFindUtil(arr, mid+1, y, n);
-    }
-    int peakIndexInMountainArray(vector<int>& A) {
-        int n = A.size();
-        return peakFindUtil(A,0,n-1,n);
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int a = 0; int b = arr.size()-1;
+        while(a<b){
+            int m = (a+b)/2;
+            if(arr[m]>arr[m+1])b=m;
+            else a=m+1;
+        }
+        return a;
     }
