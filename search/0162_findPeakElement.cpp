166b7cd52e12cfcb1aa06b8223ac5be1df3a570b
zephyr-bj <zephyrutd@gmail.com>
2020-11-06 09:46:39 -0800

    Update 0162_findPeakElement.cpp

--- search/0162_findPeakElement.cpp
+++ search/0162_findPeakElement.cpp
@@ -1,20 +1,13 @@
     int findPeakElement(vector<int>& nums) {
         int n = nums.size();
-        int i = 0; int j=n-1;
-        while(i<j){
-            int m = (i+j)/2;
-            bool peak = false;
-            if(m==0){
-                if(nums[m]>nums[m+1])return 0;
-                else i=m+1;
-            }else if(m==n-1){
-                if(nums[m]>nums[m-1])return n-1;
-                else j=m-1;
+        int a = 0; int b = n-1;
+        while(a<b){
+            int m = (a+b)/2;
+            if(nums[m]<nums[m+1]){
+                a = m+1;
             }else{
-                if(nums[m]>nums[m-1]&&nums[m]>nums[m+1])return m;
-                else if(nums[m]>nums[m-1])i=m+1;
-                else j=m-1;
-            } 
+                b = m;
+            }
         }
-        return i;
+        return a;
     }
