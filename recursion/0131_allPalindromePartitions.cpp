36a8c38c4b88c1f6eea92e91bf58d2a68b13c7e9
zephyr-bj <zephyrutd@gmail.com>
2020-11-20 08:56:28 -0800

    Update 0131_allPalindromePartitions.cpp

--- recursion/0131_allPalindromePartitions.cpp
+++ recursion/0131_allPalindromePartitions.cpp
@@ -1,18 +1,17 @@
+    bool isPalind(string s){
+        int x = 0; int y = s.size()-1;
+        while(x<y){
+            if(s[x++]!=s[y--])return false;
+        }
+        return true;
+    }
     void PTTool(string & s, int p, vector<string>&seq, vector<vector<string>>&ans){
         if(p==s.size()){
             ans.push_back(seq);
             return;
         }
         for(int i=p; i<s.size(); i++){
-            int x=p; int y=i;
-            bool isPalind=true;
-            while(x<y){
-                if(s[x]!=s[y]){
-                    isPalind=false; break;
-                }
-                x++;y--;
-            }
-            if(isPalind){
+            if(isPalind(s.substr(p,i-p+1))){
                 seq.push_back(s.substr(p,i-p+1));
                 PTTool(s, i+1, seq, ans);
                 seq.pop_back();
