135231caeaaa553f745d41d31c3bd53b1bb4fae2
zephyr-bj <zephyrutd@gmail.com>
2020-11-20 21:24:46 -0800

    Update 0037_sudokuSolver.cpp

--- recursion/0037_sudokuSolver.cpp
+++ recursion/0037_sudokuSolver.cpp
@@ -1,26 +1,24 @@
-    bool sudokuTool(vector<vector<char>>& board){
-        for(int i=0; i<9; i++){
-            for(int j=0; j<9; j++){
-                if(board[i][j]=='.'){
-                    vector<bool>tofind(9,0);
-                    for(int k=0; k<9; k++){
-                        if(board[i][k]!='.')tofind[board[i][k]-'1']=1;
-                        if(board[k][j]!='.')tofind[board[k][j]-'1']=1;
-                        if(board[(i/3)*3+k/3][(j/3)*3+k%3]!='.')tofind[board[(i/3)*3+k/3][(j/3)*3+k%3]-'1']=1;
-                    }
-                    for(int k=0; k<9; k++){
-                        if(tofind[k]==0){
-                            board[i][j]=k+'1';
-                            if(sudokuTool(board))return true;
-                            else board[i][j]='.';
-                        }
-                    }
-                    return false;
-                }
+    bool solve(vector<vector<char>>&board, int i, int j){
+        if(i==0 && j==9)return true;
+        int x = (i+1)%9;
+        int y = j+(i+1)/9;
+        if(board[i][j]!='.')return solve(board,x,y);
+        for(int a=1; a<=9; a++){
+            int good = true;
+            for(int k=0; k<9; k++){
+                if(board[i][k]=='0'+a)good = false;
+                if(board[k][j]=='0'+a)good = false;
+                if(board[(i/3)*3+k/3][(j/3)*3+k%3]=='0'+a)good=false;
+                if(!good)break;
+            }
+            if(good){
+                board[i][j]='0'+a;
+                if(solve(board,x,y))return true;
+                board[i][j]='.';
             }
         }
-        return true;
+        return false;
     }
     void solveSudoku(vector<vector<char>>& board) {
-        sudokuTool(board);
+        solve(board,0,0);
     }
