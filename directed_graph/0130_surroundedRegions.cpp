cccb1248449a4acd02af737306e51134b92d156b
zephyr-bj <zephyrutd@gmail.com>
2020-11-21 22:23:23 -0800

    Update 0130_surroundedRegions.cpp

--- directed_graph/0130_surroundedRegions.cpp
+++ directed_graph/0130_surroundedRegions.cpp
@@ -1,31 +1,30 @@
-   void dfs(vector<vector<char>>& board,int i, int j){
-        if(i<0 || i >= board.size()|| j <0 || j >= board[i].size())
-            return;
-        if(board[i][j]!='O')
-            return;
-        board[i][j] = '$';
-        dfs(board,i-1,j);
-        dfs(board,i+1,j);
-        dfs(board,i,j-1);
-        dfs(board,i,j+1);
+    void cover(vector<vector<char>>&board, int i, int j, int n, int m){
+        if(i<0||j<0||i>=n||j>=m||board[i][j]!='O')return;
+        board[i][j]='Y';
+        cover(board,i-1,j,n,m);
+        cover(board,i+1,j,n,m);
+        cover(board,i,j-1,n,m);
+        cover(board,i,j+1,n,m);
     }
     void solve(vector<vector<char>>& board) {
-        for(int i = 0;i<board.size();i++){
-            for(int j =0;j<board[i].size();j++){
-               if(i == 0 || i == board.size()-1 || j == 0 || j == board[i].size()-1){
-                   dfs(board,i,j);
-               }
+        int n = board.size();
+        if(n<1)return;
+        int m = board[0].size();
+        if(m<1)return;
+        for(int i=0; i<n; i++){
+            if(board[i][0]=='O')cover(board,i,0,n,m);
+            if(board[i][m-1]=='O')cover(board,i,m-1,n,m);
+        }
+        for(int j=0; j<m; j++){
+            if(board[0][j]=='O')cover(board,0,j,n,m);
+            if(board[n-1][j]=='O')cover(board,n-1,j,n,m);
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(board[i][j]=='O')board[i][j]='X';
+                if(board[i][j]=='Y')board[i][j]='O';
             }
         }
-         for(int i = 0;i<board.size();i++){
-            for(int j =0;j<board[i].size();j++){
-                if(board[i][j]=='O'){
-                    board[i][j] = 'X';
-                }else if(board[i][j]=='$'){
-                   board[i][j]='O' ;
-                }
-            }
-         } 
     }
     /*
     DFS
