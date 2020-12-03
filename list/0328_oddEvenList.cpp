6641bbde9f9e35645db38e8a1fcf332380eae0e1
zephyr-bj <zephyrutd@gmail.com>
2020-12-03 10:31:33 -0800

    Update 0328_oddEvenList.cpp

--- list/0328_oddEvenList.cpp
+++ list/0328_oddEvenList.cpp
@@ -13,3 +13,16 @@
         p->next=dummy.next;
         return head;
     }
+
+    ListNode* oddEvenList_1(ListNode* head) {
+        if(!head) return head;
+        ListNode *odd=head, *evenhead=head->next, *even = evenhead;
+        while(even && even->next){
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+            odd = odd->next;
+            even = even->next;
+        }
+        odd->next = evenhead;
+        return head;
+    }
