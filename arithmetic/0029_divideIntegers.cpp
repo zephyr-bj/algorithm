928fa0c85ca816475083d8ab9ec822c1f97fb1eb
zephyr-bj <zephyrutd@gmail.com>
2020-12-05 22:04:18 -0800

    Update and rename Divide-Two-Integers.cpp to 0029_divideIntegers.cpp

---
+++ arithmetic/0029_divideIntegers.cpp
@@ -0,0 +1,24 @@
+/*
+ * Divide-Two-Integers.cpp
+ *
+ *  Created on: Mar 2, 2017
+ *  Updated on: Dec 5, 2020
+ *      Author: Zephyr
+ */
+    int divide(int dividend, int divisor) {
+        if (dividend == INT_MIN && divisor == -1) {
+            return INT_MAX;
+        }
+        long dvd = labs(dividend), dvs = labs(divisor), ans = 0;
+        int sign = dividend > 0 ^ divisor > 0 ? -1 : 1;
+        while (dvd >= dvs) {
+            long temp = dvs, m = 1;
+            while (temp << 1 <= dvd) {
+                temp <<= 1;
+                m <<= 1;
+            }
+            dvd -= temp;
+            ans += m;
+        }
+        return sign * ans;
+    }
