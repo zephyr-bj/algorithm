fcf4bab6b42b9e4f15256bb31e09f3b50bc1b281
zephyr-bj <zephyrutd@gmail.com>
2020-12-06 00:09:26 -0800

    Update Plus-One.cpp

--- arithmetic/Plus-One.cpp
+++ arithmetic/Plus-One.cpp
@@ -2,27 +2,20 @@
  * Plus-One.cpp
  *
  *  Created on: Mar 18, 2017
+ *  Updated on: Dec 06, 2020
  *      Author: Zephyr
  */
-#include <vector>
-using namespace std;
-
     vector<int> plusOne(vector<int>& digits) {
-        vector<int>ans;
-        int n = digits.size();
-        int i=n-1;
-        int c=0;
-        while(i>=0){
-            int b=0;
-            if(i==n-1)b=1;
-            int s = c+digits[i]+b;
-            digits[i]=s%10;
-            c=s/10;
-            if(c==0)break;
-            i--;
-        }
-        if(c!=0)ans.push_back(c);
-        for(int i=0; i<n; i++)ans.push_back(digits[i]);
-
-        return ans;
+	    int n = digits.size();
+	    for (int i = n - 1; i >= 0; --i){
+		    if (digits[i] == 9){
+			    digits[i] = 0;
+		    }else{
+			    digits[i]++;
+			    return digits;
+		    }
+	    }
+		digits[0] =1;
+		digits.push_back(0);
+		return digits;
     }
