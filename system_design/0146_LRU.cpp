d87cc3d3971e04bbbe5f1ec169831bee37d25eb6
zephyr-bj <zephyrutd@gmail.com>
2020-12-07 12:47:24 -0800

    Update 0146_LRU.cpp

--- system_design/0146_LRU.cpp
+++ system_design/0146_LRU.cpp
@@ -1,37 +1,31 @@
 class LRUCache {
 public:
-    int depth;
-list<vector<int>>que;
-unordered_map<int, list<vector<int>>::iterator>proj;
-
+    list<pair<int,int>>cache;
+    unordered_map<int,list<pair<int,int>>::iterator>dict;
+    int cap;
     LRUCache(int capacity) {
-        depth=capacity;
+        cap = capacity;
     }
     
     int get(int key) {
-        if(proj.find(key)==proj.end()){
-            return -1;
+        if(dict.count(key)){
+            int x = dict[key]->second;
+            cache.push_front(*dict[key]);
+            cache.erase(dict[key]);
+            dict[key]=cache.begin();
+            return x;
         }else{
-            vector<int>pair=*proj[key];
-            que.erase(proj[key]);
-            que.push_front(pair);
-            proj[key]=que.begin();
-            return pair[1];
-        }    
+            return -1;
+        }
     }
     
     void put(int key, int value) {
-        if(proj.find(key)==proj.end()){
-            if(que.size()==depth){
-                proj.erase(proj.find(que.back()[0]));
-                que.pop_back();
-            }
-        }else{
-            que.erase(proj[key]);
+        if(dict.count(key))cache.erase(dict[key]);
+        cache.push_front(pair<int,int>({key,value}));
+        dict[key]=cache.begin();
+        if(cache.size()>cap){
+            dict.erase(cache.back().first);
+            cache.pop_back();
         }
-        vector<int>pair(2,key);
-        pair[1]=value;
-        que.push_front(pair);
-        proj[key]=que.begin();
     }
 };
