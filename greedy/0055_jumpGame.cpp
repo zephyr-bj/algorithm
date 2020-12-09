268d86d382258fe85e6925e6ec4a5e4a067dcc66
zephyr-bj <zephyrutd@gmail.com>
2020-12-08 22:04:19 -0800

    Update 0055_jumpGame.cpp

--- greedy/0055_jumpGame.cpp
+++ greedy/0055_jumpGame.cpp
@@ -1,16 +1,9 @@
-    bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        int i = 0;
-        while(i<n-1){
-            int x=0;
-            int s=0;
-            for(int t=1; i+t<n&&t<=nums[i]; t++){
-                if(t+nums[i+t]>s){
-                    s=t+nums[i+t];x=t;
-                }
-            }
-            if(x<1)return false;
-            i+=x;
-        }
-        return true;
+bool canJump(vector<int>& nums) {
+    const int n = nums.size();
+    int far = nums[0];
+    for (int i = 0; i < n; i++) {
+        if (i > far) break;
+        far = max(far, i + nums[i]);
     }
+    return far >= n-1;
+}
