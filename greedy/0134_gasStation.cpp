ce7cffbd09d4f10ac943868a782f6473eb163408
zephyr-bj <zephyrutd@gmail.com>
2020-12-08 22:07:53 -0800

    Update 0134_gasStation.cpp

--- greedy/0134_gasStation.cpp
+++ greedy/0134_gasStation.cpp
@@ -1,12 +1,14 @@
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         int ans = 0;
         int sum = 0;
+        int gap=0;
         for(int i=0; i<gas.size(); i++){
             sum+=(gas[i]-cost[i]);
             if(sum<0){
+                gap+=sum;
                 ans=i+1;
                 sum=0;
             }
         }
-        return sum>0?ans:-1;
+        return sum+gap>=0?ans:-1;
     }
