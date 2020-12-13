7a1df3dcb85f41d3091eac3ed2a835ae81e0b3e9
zephyr-bj <zephyrutd@gmail.com>
2020-12-12 21:40:26 -0800

    Update Merge-Intervals.cpp

--- misc/Merge-Intervals.cpp
+++ misc/Merge-Intervals.cpp
@@ -16,26 +16,17 @@ struct Interval {
    Interval(int s, int e) : start(s), end(e) {}
 };
 
-class Solution {
-public:
-    static bool cmp(Interval a,Interval b){return (a.start < b.start);}
-    vector<Interval> merge(vector<Interval>& intervals) {
-        if(intervals.size()==0)return intervals;
-        sort(intervals.begin(), intervals.end(), cmp);
-        vector<Interval>ans;
-        int j=0;
-        for(int i=j+1; i<intervals.size(); i++){
-            if(intervals[i].start<=intervals[j].end){
-                if(intervals[i].end>intervals[j].end){
-                    intervals[j].end=intervals[i].end;
-                }
-            }else{
-                ans.push_back(intervals[j]);
-                j=i;
-            }
-        }
-        ans.push_back(intervals[j]);
-        return ans;
+vector<Interval> merge(vector<Interval>& ins) {
+    if (ins.empty()) return vector<Interval>{};
+    vector<Interval> res;
+    sort(ins.begin(), ins.end(), [](Interval a, Interval b){return a.start < b.start;});
+    res.push_back(ins[0]);
+    for (int i = 1; i < ins.size(); i++) {
+        if (res.back().end < ins[i].start) res.push_back(ins[i]);
+        else
+            res.back().end = max(res.back().end, ins[i].end);
     }
-};
+    return res;
+}
+
 
