5ce3cbcd10a188d0ec0ebe914e99ca188da5a4c9
zephyr-bj <zephyrutd@gmail.com>
2020-12-16 07:57:16 -0800

    Create sync_io_k.cpp

---
+++ concurrency/sync_io_k.cpp
@@ -0,0 +1,114 @@
+/* complete version of synchronous read and write, works for:
+   the no data case
+   the multiple read clients case (wait_event_interruptible waken up, return zero, but see empty, dev->rp == dev->wp again)
+*/
+
+struct my_x_dev {
+    in_db *inque;
+    int inhead, intail, insz;
+    wait_queue_head_t inwq, outwq;
+    spinlock_t inlock, outlock;
+    struct tasklet_struct in_tasklet;
+    
+    out_db * outque;
+    int outhead, outtail, outsz;
+    wait_queue_head_t outwq;
+    spinlock_t outlock;
+    
+    struct hw_x_dev hdev;  
+}
+
+/*         read part         */
+static ssize_t my_x_dev_read (struct file *filp, char __user *buf, size_t count, loff_t *f_pos){
+    struct my_x_dev *dev = filp->private_data;
+    
+    spin_lock(&dev->inlock);
+    while (dev->intail == dev->inhead) {
+        spin_unlock_irqsave(&dev->inlock);
+        if (filp->f_flags & O_NONBLOCK)
+            return -EAGAIN;
+        wait_event_interruptible(dev->inwq, (dev->intail != dev->inhead));
+        spin_lock_irqrestore(&dev->inlock);
+    }
+    
+    /* ok, data is there, return something */
+    if (dev->inhead > dev->intail)count = min(count, (size_t)(dev->inhead - dev->intail));
+    else count = min(count, (size_t)(dev->insz - dev->intail + 1));                        
+    if (copy_to_user(buf, dev->inque+dev->intail, count*sizeof(in_db))) {
+        spin_unlock_irqrestore(&dev->inlock);
+        return -EFAULT;
+    }                        
+    dev->inrp = (dev->inrp+count)%dev->insz;
+    spin_unlock_irqrestore(&dev->inlock);
+    return count;
+}
+
+static irqreturn_t my_x_dev_in_irs_fast(int irq, void *dev_id, struct pt_regs *regs){
+    my_x_dev * dev = (my_x_dev*)dev_id;
+    unsigned long flags;
+    spin_lock_irqsave(&(dev->inlock),flags);
+    dev->inhead = (dev->inhead+1)%dev->insz;
+    if(dev->inhead == dev->intail) dev->intail = (dev->intail+1)%dev->insz;
+    hw_get_data(dev->inque+dev->inhead);
+    spin_lock_irqrestore(&(dev->inlock));
+    wake_up_interruptible(&dev->inq);
+    return IRQ_HANDLED;
+}
+
+static irqreturn_t my_x_dev_in_irs_slow(int irq, void *dev_id, struct pt_regs *regs){
+    my_x_dev * dev = (my_x_dev*)dev_id;
+    tasklet_schedule(&dev->in_tasklet);
+    return IRQ_HANDLED;
+}
+void my_x_dev_in_tasklet(unsigned long dev_id){
+    my_x_dev * dev = (my_x_dev*)dev_id;
+    unsigned long flags;
+    spin_lock_irqsave(&(dev->inlock),flags);
+    dev->inhead = (dev->inhead+1)%dev->insz;
+    if(dev->inhead == dev->intail) dev->intail = (dev->intail+1)%dev->insz;
+    hw_get_data(dev->inque+dev->inhead);
+    spin_lock_restore(&(dev->inlock));
+}
+
+/*            write part           */
+
+/* How much space is free? */
+static int spacefree(struct my_x_dev *dev){
+    if (dev->outtail == dev->outhead) return dev->outsz - 1;
+    return ((dev->outtail + dev->outsz - dev->outhead) % dev->outsz) - 1;
+}
+static ssize_t my_x_dev_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos){
+    struct my_x_dev *dev = filp->private_data;
+    int result;
+    unsigned long flags;
+    spin_lock_irqsave(&dev->outlock,flags);
+    while (spacefree(dev) == 0) { /* full */ 
+        spin_unlock_irqrestore(&dev->outlock,flags);
+        if (filp->f_flags & O_NONBLOCK)
+            return -EAGAIN;
+        wait_event_interruptible(dev->outwq, (dev->outtail != dev->outhead+1));
+        spin_lock_irqsave(&dev->outlock,flags);
+    }
+
+    count = min(count, (size_t)spacefree(dev));
+    if (dev->outhead >= dev->outtail)count = min(count, (size_t)(dev->end - dev->outhead)); 
+    else count = min(count, (size_t)(dev->outtail - dev->outhead - 1));
+    if (copy_from_user(dev->outque+dev->outhead, buf, count*sizeof(out_db))) {
+        spin_lock_irqrestore (&dev->outlock,flags);
+        return -EFAULT;
+    }
+    dev->outhead += (dev->outhead+count)%dev->outsz;
+    spin_lock_irqrestore(&dev->outlock);
+    hw_put_data(dev->outque+dev->outtail);
+    return count;
+}
+
+static irqreturn_t my_x_dev_out_irs(int irq, void *dev_id, struct pt_regs *regs){
+    my_x_dev * dev = (my_x_dev*)dev_id;
+    unsigned long flags;
+    spin_lock_irqsave(&(dev->outlock),flags);
+    dev->outtail = (dev->outtail+1)%dev->insz;
+    spin_lock_irqrestore(&(dev->outlock));
+    wake_up_interruptible(&dev->outwq);
+    return IRQ_HANDLED;
+}
