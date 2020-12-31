d72b053811224f57c5ab75c35803bf9a1a402b5e
zephyr-bj <zephyrutd@gmail.com>
2020-12-31 08:31:48 -0800

    Update 0324_wiggleSortII.cpp

--- array/0324_wiggleSortII.cpp
+++ array/0324_wiggleSortII.cpp
@@ -25,22 +25,21 @@ void wiggleSort(vector<int>& nums) {
 (3) the medians are put into the remaining slots.
 (4) the sequence A(i) gives such a sequence  
 */
-void wiggleSort(vector<int>& nums) {
-    # define A(i) nums[((1+i*2)%(n|1))]
-    auto midptr = nums.begin() + n / 2;
-    nth_element(nums.begin(), midptr, nums.end());
-    int pivot = *midptr;
-    int i=0; int j=0; int k=n-1;
-    while(j<=k){
-        if(A(j)<pivot){
-            swap(A(j),A(k--));
-        }else if(A(j)>pivot){
-            swap(A(j++),A(i++));
-        }else{
-            j++;
+    void wiggleSort(vector<int>& nums) {
+        #define idx(j) (j*2+1)%(n|1)//virtue index
+        int n = nums.size();
+        nth_element(nums.begin(),nums.begin()+n/2,nums.end());
+        int pivot = *(nums.begin()+n/2);
+        int i=0, j=0, k=n-1;
+        while(j<=k){
+            if(nums[idx(j)]<pivot){
+            }else if(nums[idx(j)]>pivot){
+                swap(nums[idx(j++)],nums[idx(i++)]);
+            }else{
+                j++;
+            }
         }
     }
-}
 /*solution 3*/
     void partition3Way(vector<int>&arr, int l, int r, int& x, int&y) { 
         int pivot = arr[r];
@@ -87,26 +86,56 @@ void wiggleSort(vector<int>& nums) {
         nums = ans;
     }
 //example
-//after kthSamllest3Say, we got
-//    1, 1, 2, 2, 2, 3
-//    the pivot = 2, 
-//    then we have
-// 1 1 2 2 2 3    j=0; i=0; k=5; nums[1]<pivot, high to 4| 2 0 nums[4]
-// 1 2 2 2 1 3    j=0; i=0; k=4; nums[1]=pivot
-// 1 2 2 2 1 3    j=1; i=0; k=4; nums[3]=pivot
-// 1 2 2 2 1 3    j=2; i=0; k=4; nums[5]>pivot, low  to 1| 3 5 nums[1]
-// 1 3 2 2 1 2    j=3; i=1; k=4; nums[0]<pivot, high to 4 2| 0 nums[2]
-// 2 3 1 2 1 2    j=3; i=1; k=3;
-/* 1, 5, 1, 1, 6, 4
-after kthSamllest3Say, we got
-    1, 1, 1, 4, 5, 6, 
-    the pivot = 1;
-    then, we have
-i=0, j=0, k=5    1, 1, 1, 4, 5, 6, 
-i=0, j=1, k=5    1, 1, 1, 4, 5, 6, 
-i=1, j=2, k=5    1, 4, 1, 1, 5, 6, 
-i=2, j=3, k=5    1, 4, 1, 6, 5, 1, 
-i=2, j=4, k=5    1, 4, 1, 6, 5, 1, 
-i=2, j=5, k=5    1, 4, 1, 6, 5, 1, 
-                 1, 4, 1, 6, 1, 5
+/*
+[1 2 3 4 5 6 7 8 9 1011]
+pivot=6
+2    1    5    4    3    6    7    8    9    10    11    0(1)    10(10)    0(1)case 1
+2    11    5    4    3    6    7    8    9    10    1    0(1)    9(8)    0(1)case 2
+2    11    5    4    3    6    7    8    9    10    1    1(3)    9(8)    1(3)case 1
+2    11    5    9    3    6    7    8    4    10    1    1(3)    8(6)    1(3)case 2
+2    11    5    9    3    6    7    8    4    10    1    2(5)    8(6)    2(5)case 3
+2    11    5    9    3    6    7    8    4    10    1    3(7)    8(6)    2(5)case 2
+2    11    5    9    3    8    7    6    4    10    1    4(9)    8(6)    3(7)case 2
+2    11    5    9    3    8    7    10    4    6    1    5(0)    8(6)    4(9)case 1
+7    11    5    9    3    8    2    10    4    6    1    5(0)    7(4)    4(9)case 2
+6    11    5    9    3    8    2    10    4    7    1    6(2)    7(4)    5(0)case 1
+6    11    3    9    5    8    2    10    4    7    1    6(2)    6(2)    5(0)
+[1 2 3 4 5 6 7 8 9 10]
+pivot=6
+2    1    5    4    3    6    7    8    9    10    0(1)    9(8)    0(1)case 1
+2    9    5    4    3    6    7    8    1    10    0(1)    8(6)    0(1)case 2
+2    9    5    4    3    6    7    8    1    10    1(3)    8(6)    1(3)case 1
+2    9    5    7    3    6    4    8    1    10    1(3)    7(4)    1(3)case 2
+2    9    5    7    3    6    4    8    1    10    2(5)    7(4)    2(5)case 3
+2    9    5    7    3    6    4    8    1    10    3(7)    7(4)    2(5)case 2
+2    9    5    7    3    8    4    6    1    10    4(9)    7(4)    3(7)case 2
+2    9    5    7    3    8    4    10    1    6    5(0)    7(4)    4(9)case 1
+3    9    5    7    2    8    4    10    1    6    5(0)    6(2)    4(9)case 1
+[1 1 2 1 2 2 1]
+pivot=1
+1    1    1    1    2    2    2    0(1)    6(6)    0(1)case 3
+1    1    1    1    2    2    2    1(3)    6(6)    0(1)case 3
+1    1    1    1    2    2    2    2(5)    6(6)    0(1)case 2
+1    2    1    1    2    1    2    3(0)    6(6)    1(3)case 3
+1    2    1    1    2    1    2    4(2)    6(6)    1(3)case 3
+1    2    1    1    2    1    2    5(4)    6(6)    1(3)case 2
+1    2    1    2    1    1    2    6(6)    6(6)    2(5)case 2
+1    2    1    2    1    2    1    7(1)    6(6)    3(0)
+[1 2 2 1 2 2 1]
+pivot=2
+1    1    1    2    2    2    2    0(1)    6(6)    0(1)case 1
+1    2    1    2    2    2    1    0(1)    5(4)    0(1)case 3
+1    2    1    2    2    2    1    1(3)    5(4)    0(1)case 3
+1    2    1    2    2    2    1    2(5)    5(4)    0(1)case 3
+1    2    1    2    2    2    1    3(0)    5(4)    0(1)case 1
+2    2    1    2    1    2    1    3(0)    4(2)    0(1)case 3
+2    2    1    2    1    2    1    4(2)    4(2)    0(1)case 1
+2    2    1    2    1    2    1    4(2)    3(0)    0(1)
+
+idx(k) starts from the very last even element, decrease by 2
+idx(j), starts from the very first odd element, increase by 2, so does idx(i), 
+when idx(j) hit the last or second last element, it goes back to 0 and moves from the lower even 
+
+
+
 */
