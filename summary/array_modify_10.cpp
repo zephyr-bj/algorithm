48fe2b9af5481e22e42564f5a12f6f9d2b511cb4
zephyr-bj <zephyrutd@gmail.com>
2021-03-07 20:37:16 -0800

    Update and rename array_modify_9.cpp to array_modify_10.cpp

--- summary/array_modify_10.cpp
+++ summary/array_modify_10.cpp
@@ -1,8 +1,7 @@
-/* sort array [4] (0164) bucket sort (0215) quick sort (0324) wiggle sort II (0179) largest number string 
+/* sort array [4] (0164) bucket sort (0215) quick sort (0324) wiggle sort II (0179*) largest number string 
                   partition (*) 3-way partition (*)
- * reverse array[2] (0344) reverse string (0345) reverse vowels 
- * rotate array [1] (0189)
- * merge array [1] (0088)
+ * reverse array[3] (0344) reverse string (0345*) reverse vowels (0189*) rotate array
+ * merge array [2] (0088) merge two arrays (0313) super ugly number
  * shuffle array [1] (0384)
  */
 //maximum gap (0164)
@@ -49,36 +48,51 @@
         return ans;
     }
 //Kth largest element (0215)
-    // we need the partition function
-    int findKthLargest(vector<int>& nums, int k) {
-        int n = nums.size();
-        int left = 0, right = nums.size() - 1;
-        while (true) {
-            int idx = partition(nums, left, right);
-            if (idx == n-k) {
-                break;
-            }else if (idx < n-k) {
-                left = idx + 1;
-            } else {
-                right = idx - 1;
+    // C implementation, we need the partition function
+    int findKthLargest(int* nums, int numsSize, int k){
+        int a = 0;
+        for(int b=0; b<numsSize-1; b++){
+            if(nums[b] < nums[numsSize-1]){
+                int t = nums[a];
+                nums[a++]=nums[b];
+                nums[b]=t;
             }
         }
-        return nums[n-k];
+        int t = nums[a];
+        nums[a]=nums[numsSize-1];
+        nums[numsSize-1]=t;
+        if(numsSize-k == a)return nums[a];
+        else if(numsSize-k > a)return findKthLargest(nums+a+1, numsSize-a-1, k);
+        else return findKthLargest(nums,a,k-(numsSize-a));
+    }
+// C++ implementation, use heap
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(auto x:nums){
+            if(pq.size()<k)pq.push(x);
+            else{
+                if(pq.top()<x){
+                    pq.pop();
+                    pq.push(x);
+                }
+            }
+        }
+        return pq.top();
     }
 //wiggle sort II (0324)
-void wiggleSort(vector<int>& nums) {
-    sort(nums.begin(), nums.end());
-    int n = nums.size();
-    int j = (n + 1) / 2, k = n;
-    vector<int> cp(nums);
-    for (int i = 0; i < n; ++i) {
-        if (i % 2 == 0) {
-            nums[i] = cp[--j];
-        } else {
-            nums[i] = cp[--k];
+    void wiggleSort(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int j = (n + 1) / 2, k = n;
+        vector<int> cp(nums);
+        for (int i = 0; i < n; ++i) {
+            if (i % 2 == 0) {
+                nums[i] = cp[--j];
+            } else {
+                nums[i] = cp[--k];
+            }
         }
     }
-}
     void wiggleSort(vector<int>& nums) {
         #define idx(j) (j*2+1)%(n|1)//virtue index
         int n = nums.size();
@@ -142,6 +156,7 @@ string reverseVowels(string s) {
     int L=s.size();
     int i=0; int j=L-1;
     while(i<j){
+        // have to make sure "i<j" any time
         while((!isVowel(s[i]))&&i<j)i++;
         while((!isVowel(s[j]))&&j>i)j--;
         swap(s[i],s[j]);
@@ -175,14 +190,25 @@ string reverseVowels(string s) {
             k--;
         }
     }  
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i=m-1;
-        int j=n-1;
-        int p=m+n-1;
-        while(p>=0){
-            if(j<0||(i>=0&&nums1[i]>=nums2[j]))swap(nums1[i--],nums1[p--]);
-            else swap(nums2[j--],nums1[p--]);
+// (0313) super ugly number
+// n way merge
+    int nthSuperUglyNumber(int n, vector<int>& primes) {
+        int k = primes.size();
+        vector<int>index(k,0);
+        vector<int>result(n,INT_MAX);
+        result[0]=1;
+        for(int j=0; j<n; j++){
+            for(int i=0; i<k; i++){
+                if(result[j]>primes[i]*result[index[i]]){
+                    result[j] = primes[i]*result[index[i]]; 
+                }
+            }
+            for(int i=0; i<k; i++){
+                if(result[j]==primes[i]*result[index[i]])
+                    index[i]+=1;
+            }
         }
+        return result[n-1];
     }
 //shuffle array (0384)
     vector<int> shuffle() {
