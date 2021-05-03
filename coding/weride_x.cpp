850d148f9cbc47221bd06d5d8b572f97bf04685b
zephyr-bj <zephyrutd@gmail.com>
2021-05-03 14:53:48 -0700

    Update weride_x.cpp

--- coding/weride_x.cpp
+++ coding/weride_x.cpp
@@ -1,4 +1,9 @@
 // (0505) maze II
+// (0621) task Scheduler
+// (0362) Design a hit counter
+// (0339) nested list weight sum
+
+// (0505) maze II
     int shortestDistance(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
         int m = maze.size(), n = maze[0].size();
         vector<vector<int>> dists(m, vector<int>(n, INT_MAX));
@@ -57,3 +62,135 @@
         int res = dists[destination[0]][destination[1]];
         return (res == INT_MAX) ? -1 : res;
     }
+// (0621) task Scheduler
+/*  when all tasks are unique, they are all well seperated
+ *  1. The biggest problem comes from the most repeated task, say it's task A
+ *  2. we can put all A tasks into the array, and make sure they are separated by n idle inervals. gap = (num(A) - 1) x n
+ *  3. Now we need to replace the idle intervals with other tasks
+ *     1) basic length (num(A)-1) x (n+1)
+ *     2) for task A (num(A)-1) x (n+1) + 1;
+ *     3) for all the tasks as many as A, (num(A)-1) x (n+1) + m
+ */
+    int leastInterval(vector<char>& tasks, int n) {
+        unordered_map<char,int>mp;
+        int count = 0;
+        for(auto e : tasks){
+            mp[e]++;
+            count = max(count, mp[e]);
+        }
+        
+        int ans = (count-1)*(n+1);
+        for(auto e : mp) if(e.second == count) ans++;
+        return max((int)tasks.size(), ans);
+    }
+
+// (0362) Design a hit counter
+class HitCounter {
+public:
+    /** Initialize your data structure here. */
+    HitCounter() {}
+    
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    void hit(int timestamp) {
+        q.push(timestamp);
+    }
+    
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    int getHits(int timestamp) {
+        while (!q.empty() && timestamp - q.front() >= 300) {
+            q.pop();
+        }
+        return q.size();
+    }
+
+private:
+    queue<int> q;
+};
+
+class HitCounter {
+public:
+    /** Initialize your data structure here. */
+    HitCounter() {
+        times.resize(300);
+        hits.resize(300);
+    }
+    
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    void hit(int timestamp) {
+        int idx = timestamp % 300;
+        if (times[idx] != timestamp) {
+            times[idx] = timestamp;
+            hits[idx] = 1;
+        } else {
+            ++hits[idx];
+        }
+    }
+    
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    int getHits(int timestamp) {
+        int res = 0;
+        for (int i = 0; i < 300; ++i) {
+            if (timestamp - times[i] < 300) {
+                res += hits[i];
+            }
+        }
+        return res;
+    }
+
+private:
+    vector<int> times, hits;
+};
+
+// (0339) nested list weight sum
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Constructor initializes an empty nested list.
+ *     NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     NestedInteger(int value);
+ *
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     void add(const NestedInteger &ni);
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+class Solution {
+public:
+    int depthSum(vector<NestedInteger>& nestedList) {
+        return dfs(nestedList, 1);
+    }
+    int dfs(vector<NestedInteger>& nestedList, int depth) {
+        if (nestedList.empty())
+            return 0;
+        int res = 0;
+        for (auto& nest : nestedList) {
+            if (nest.isInteger()) {
+                res += nest.getInteger() * depth;
+            } else {
+                res += dfs(nest.getList(), depth + 1);
+            }
+        }
+        return res;
+    }
+};
