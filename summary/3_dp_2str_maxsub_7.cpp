051e4b1e967b6ccae3ea38f5ec4baef0cce0e9f2
zephyr-bj <zephyrutd@gmail.com>
2021-05-28 12:49:38 -0700

    Update 3_dp_2str_maxsub_7.cpp

--- summary/3_dp_2str_maxsub_7.cpp
+++ summary/3_dp_2str_maxsub_7.cpp
@@ -131,3 +131,80 @@
         return ans;
     }
 
+/* Kadane's algorithm  
+int maxSubArray(vector<int>& nums, int &start, int &end) {
+    int n = nums.size();
+    if(n<1)return 0;
+    int sum = nums[0];
+    int ans = nums[0];
+    int new_start 
+    for(int i= 1; i<n; i++){
+        if (sum+nums[i] < nums[i])
+            new_start = i;
+        sum = max(sum+nums[i],nums[i]);
+        if (ans < sum) {
+            start = new_start;
+            end = i;
+        }
+        ans = max(sum,ans);
+    }
+    return ans;
+}
+// solution from geeks4geeks
+int kadane(int* arr, int* start, int* finish, int n)  {  
+    int sum = 0, maxSum = INT_MIN, i;  
+    *finish = -1;  
+    int local_start = 0;  
+    for (i = 0; i < n; ++i)  {  
+        sum += arr[i];  
+        if (sum < 0)  {  
+            sum = 0;  
+            local_start = i + 1;  
+        } else if (sum > maxSum)  {  
+            maxSum = sum;  
+            *start = local_start;  
+            *finish = i;  
+        }  
+    }  
+    if (*finish != -1)  
+        return maxSum;  
+    // Special Case: When all numbers in arr[] are negative  
+    maxSum = arr[0];  
+    *start = *finish = 0;  
+    // Find the maximum element in array  
+    for (i = 1; i < n; i++)  {  
+        if (arr[i] > maxSum)  {  
+            maxSum = arr[i];  
+            *start = *finish = i;  
+        }  
+    }  
+    return maxSum;  
+}  
+void findMaxSum(vector<vector<int>>M)  {   
+    int maxSum = INT_MIN, finalLeft, finalRight,  
+                          finalTop, finalBottom;  
+    int left, right, i;  
+    int ROW=M.size();
+    int COL=M[0].size();
+    int* temp = (int*)malloc(sizeof(int)*ROW); 
+    int sum, start, finish;  
+    // Set the left column  
+    for (left = 0; left < COL; ++left)  {  
+        memset(temp, 0, sizeof(temp));  
+        // Set the right column for the left column set by outer loop  
+        for (right = left; right < COL; ++right)  {  
+            for (i = 0; i < ROW; ++i)  
+                temp[i] += M[i][right];  
+            sum = kadane(temp, &start, &finish, ROW);
+            if (sum > maxSum)  {  
+                maxSum = sum;  
+                finalLeft = left;  
+                finalRight = right;  
+                finalTop = start;  
+                finalBottom = finish;  
+            }  
+        }  
+    }  
+    free(temp);
+}
+*/
