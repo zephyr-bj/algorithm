873ea383f9d6ba5ac9d14b097e9b6cc23fb759ad
zephyr-bj <zephyrutd@gmail.com>
2021-05-29 17:18:52 -0700

    Update and rename dp_misc_8.cpp to 3_dp_misc_8.cpp

--- summary/3_dp_misc_8.cpp
+++ summary/3_dp_misc_8.cpp
@@ -92,71 +92,76 @@
  * we use the larget element divide the question into subquestions, all nums could be a larget element of a subset. 
  * we use two fields to represents the subset: its size, the next larget element, then we can easily tell if which one is larger, and construct the subset
  */
-    vector<int> largestDivisibleSubset(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        if(n<1)return vector<int>();
-        vector<vector<int>>lds(1,vector<int>({1,-1}));
-        int idx = 0; int L = 1;
-        for(int i=1; i<n; i++){
-            int l = 1;
-            int k = -1;
-            for(int j=0; j<i; j++){
-                if((nums[i]%nums[j]==0) && (l<lds[j][0]+1)){
-                    l=lds[j][0]+1;
-                    k=j;
-                }
-            }
-            vector<int>tmp({l,k});
-            lds.push_back(tmp);
-            if(l>L){
-                idx = i; L = l;
+vector<int> largestDivisibleSubset(vector<int>& A) {
+    int n = A.size();
+    if (n == 0)
+        return {};
+ 
+    sort(A.begin(), A.end());//   so that we have to only check A[j]%A[i] and not A[i]%A[j], i < j
+    vector<int> dp(n, 1);//  dp[i] : longest subset, satisfying the required conditions and ending at index i
+    vector<int> previous_index(n, -1);
+       
+    int max_ind = 0; //this keeps track of the index at which the longest subset ends
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (A[i] % A[j] == 0 and dp[i] < dp[j] + 1) {
+                dp[i] = dp[j]+1;
+                previous_index[i] = j;
             }
         }
-        vector<int>ans;
-        while(idx!=-1){
-            ans.push_back(nums[idx]);
-            idx=lds[idx][1];
+        if (dp[i] > dp[max_ind]) {
+            max_ind = i;
         }
-        return ans;
     }
+
+    vector<int> answer;
+    int t = max_ind;  
+    while (t >= 0) {
+        answer.push_back(A[t]);
+        t = previous_index[t];
+    }
+    return answer;  
+}
     
 //(0132) min cut to form palindrome substring time O(nn), memory O(nn)
-    int minCut(string s) {
-        int n=s.size();
-        if(n<2)return 0;
-        vector<vector<bool>>plindr(n,vector<bool>(n,0));
-        vector<int>bin(1,-1);//element before 0, -1
-        int ans=n-1;
-        for(int i=0; i<n; i++){
-            int cut=i;
-             for(int j=0; j<=i; j++){
-                if(j==i||(s[j]==s[i]&&(j+1==i||plindr[j+1][i-1]))){
-                    plindr[j][i]=1;
-                    if(cut>bin[j]+1)cut=bin[j]+1;
-                }
+int minCut(string s) {
+    int n=s.size();
+    if(n<2)return 0;
+    vector<vector<bool>>plindr(n,vector<bool>(n,0));
+    vector<int>bin(1,-1);//element before 0, -1
+    int ans = n - 1;
+    for (int i = 0; i < n; i++) {
+        int cut = i;
+        for (int j = 0; j <= i; j++) {
+            if (j == i || (s[j] == s[i] && (j + 1 == i || plindr[j+1][i-1]))) {
+                plindr[j][i] = 1;
+                if(cut > bin[j] + 1)
+                    cut = bin[j] + 1;
             }
-            bin.push_back(cut);
         }
-        return bin.back();
+        bin.push_back(cut);
     }
+    return bin.back();
+}
 //(0312) max score by burst bulloons time O(nn), memory O(nn)
 int maxCoins(vector<int>& nums) {
-        int n=nums.size();
-        if(n==0)return 0;
-        vector<int>balloons(1,1);
-        for(int i=0; i<n; i++)balloons.push_back(nums[i]);
-        balloons.push_back(1);
-        vector<int>tmp(n+2,0);
-        vector<vector<int>>dp(n+2,tmp);
-        for(int len=3; len<=n+2; len++){
-            for(int l=0; l<=n+2-len; l++){
-                int r = l+len-1;
-                for(int k=l+1; k<r; k++){
-                    int x = dp[l][k]+dp[k][r]+balloons[l]*balloons[k]*balloons[r];
-                    if(dp[l][r]<x)dp[l][r]=x;
-                }
+    int n = nums.size();
+    if (n == 0)
+        return 0;
+    vector<int>balloons(1,1);
+    for(int i=0; i<n; i++)balloons.push_back(nums[i]);
+    balloons.push_back(1);
+    vector<int>tmp(n+2,0);
+    vector<vector<int>>dp(n+2,tmp);
+    for (int len = 3; len <= n + 2; len++) {
+        for (int l = 0; l <= n + 2 - len; l++) {
+            int r = l + len -1;
+            for (int k = l + 1; k < r; k++){
+                int x = dp[l][k]+dp[k][r]+balloons[l]*balloons[k]*balloons[r];
+                if (dp[l][r] < x)
+                    dp[l][r] = x;
             }
         }
-        return dp[0][n+1];
     }
+    return dp[0][n+1];
+}
