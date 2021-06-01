52ecaef22a6faf802aaa3f2a3ce7210837085d5c
zephyr-bj <zephyrutd@gmail.com>
2021-05-31 21:56:45 -0700

    Update and rename 1_array_elements_12.cpp to 1_array_elements_11.cpp

--- summary/1_array_elements_11.cpp
+++ summary/1_array_elements_11.cpp
@@ -4,17 +4,16 @@
  * find elements [5]
  * missing positive (0041) (0268) (0287)
  * find majority (0169) (0229)
- * interval intersections [1] (0986) interval Lists intersections
  */
 //remove duplicate in sorted arrary (0026)
 int removeDuplicates(vector<int>& nums) {
     int n = nums.size();
     if (n < 2)
-	    return n;
+	return n;
     int i = 0;
     for (int j = 1; j < n; j++) {
         if(nums[j] > nums[i])
-			i++;
+	        i++;
         nums[i] = nums[j];
     }
     return i+1;
@@ -26,7 +25,7 @@ int removeElement(vector<int>& nums, int val) {
     for (int j = 0; j < n; j++) {
         nums[i] = nums[j];
         if (nums[j] != val)
-			i++;
+	        i++;
     }   
     return i;
 }  
@@ -34,11 +33,11 @@ int removeElement(vector<int>& nums, int val) {
 int removeDuplicates(vector<int>& nums) {
     int n = nums.size();
     if (n < 3)
-            return n;
+        return n;
     int i = 1;
     for (int j = 2; j < n; j++) {
         if(nums[i] != nums[i-1] || nums[i] != nums[j])
-                i++;
+            i++;
         nums[i] = nums[j];
     }   
     return i+1;
@@ -49,7 +48,7 @@ void moveZeroes(vector<int>& nums) {
     for (int j = 0; j < nums.size(); j++) {
         nums[i] = nums[j];
         if(nums[i] != 0)
-			i++;
+	    i++;
     }   
     while (i < nums.size()) {
         nums[i++] = 0;
@@ -127,7 +126,8 @@ int firstMissingPositive(vector<int>& nums) {
         for(auto a:nums)y+=a;
         return x-y;
     }
-//find the duplicate number from range [1,n-1] (0287) 
+//find the duplicate number from range [1,n-1], only one repeated number (0287) 
+//    or: n+1 integer, [1,n] inclusive
 //since each value is different from the others, so if there is no duplicates, the slow pointer must iterates all the elements
 	int findDuplicate3(vector<int>& nums){
 		if (nums.size() > 1){
@@ -159,17 +159,21 @@ int firstMissingPositive(vector<int>& nums) {
     }
 //find majority element : more than 1/2 (0169)
     //moore voting
-    int majorityElement(int[] num) {
-        int major=num[0], count = 1;
-        for(int i=1; i<num.length;i++){
-            if(count==0){
-                count++;
-                major=num[i];
-            }else if(major==num[i]){
-                count++;
-            }else count--;
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int maj = 1;
+        int cnt = 0;
+        for (int i=0; i<n; i++) {
+            if(nums[i] == maj){
+                cnt++;
+            } else if (cnt == 0) {
+                maj = nums[i];
+                cnt = 1;
+            } else {
+                cnt--;
+            }
         }
-        return major;
+        return maj;
     }
 //ind majority elements : more than 1/3 (0229)
    vector<int> majorityElement(vector<int>& nums) {
@@ -198,28 +202,5 @@ int firstMissingPositive(vector<int>& nums) {
         vector<int>ans;
         if(cnt1>nums.size()/3)ans.push_back(a);
         if(cnt2>nums.size()/3)ans.push_back(b);
-        return ans;
-    }
-
-//(0986) interval Lists intersections
-    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
-        int n = firstList.size();
-        int m = secondList.size();
-        int i=0; int j=0;
-        vector<vector<int>>ans;
-        while(i<n && j<m){
-            if(firstList[i][1]< secondList[j][0])i++;
-            else if(secondList[j][1] < firstList[i][0])j++;
-            else{
-                int a = max(firstList[i][0],secondList[j][0]);
-                int b = min(firstList[i][1],secondList[j][1]);
-                if(firstList[i][1]<secondList[j][1])i++;
-                else if(firstList[i][1]>secondList[j][1])j++;
-                else{
-                    i++; j++;
-                }
-                ans.push_back(vector<int>({a,b}));
-            }
-        }
         return ans;
     }
