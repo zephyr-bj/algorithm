97b86c0537422188d256fecb1da67822d8d9744a
zephyr-bj <zephyrutd@gmail.com>
2021-06-11 18:09:04 -0700

    Update 3_iterative_extreme_16.cpp

--- summary/3_iterative_extreme_16.cpp
+++ summary/3_iterative_extreme_16.cpp
@@ -1,13 +1,15 @@
 /*
- *water [2]:  (0011) container with most water (0042) trap rain water
- *distribution match [3] (0030) Substring with Concatenation of All Words (0076) minimum window in S which will contain all the characters in T 
- *                       (0438) Find All Anagrams in a String
- *gradient match [1] (0149) max points on a line 
- *[1]:(0209) shortest subarray with sum larger than a number 
- * (0003*)Longest Substring Without Repeating Characters 
- * hash: search by hash table [2] (0128)search longest Consecutive Sequence (0274)search the H index (not calculate)
- * set: lower bound [2] : (0300) LIS; (0354) LIS: russian doll envelopes
- * increasing/decreasing stack [4] : (0239) sliding window maximum (0155) min stack (0084) largest rectangle histogram (0316) remove duplicate letters to lexicographically smallest
+ * water [2]:  (0011) container with most water (0042) trap rain water
+ *       [1]:  (0209) shortest subarray with sum larger than a number 
+ * with help of hash [7]: search by hash table  (0128) search longest Consecutive Sequence (0274)search the H index (not calculate)
+ *                                              (0003*)Longest Substring Without Repeating Characters
+ *                        distribution match    (0030) Substring with Concatenation of All Words 
+ *                                              (0076) minimum window in S which will contain all the characters in T 
+ *                                              (0438) Find All Anagrams in a String
+ *                        gradient match        (0149) max points on a line
+ * with help of set  [2]: lower bound           (0300) LIS; (0354) LIS: russian doll envelopes
+ * with help of stack[4]: increasing stack      (0239) sliding window maximum (0084) largest rectangle histogram (0316) remove duplicate letters to lexicographically smallest
+ *                        decreasing stack      (0155) min stack 
  */
     
 //(0011) container with most water 
@@ -44,6 +46,80 @@
         }
         return ans;
     }
+
+// (0209) shortest subarray with sum larger than a number 
+    int minSubArrayLen(int s, vector<int>& nums) {
+        int L = nums.size();
+        if(L<1)return 0;
+        int ans = L+1;
+        int p=0; int q=0; int sum=0;
+        while(q<L){
+            sum+=nums[q];
+            while(sum-nums[p]>=s){
+                sum-=nums[p++];
+            }
+            if(sum>=s&&q-p+1<ans)ans=q-p+1;
+            q++;
+        }
+        return ((ans>L) ? 0 : ans);
+    }
+
+/*******************************************************************************************************/
+//(0003)Longest Substring Without Repeating Characters
+    int lengthOfLongestSubstring(string s) {
+        vector<int>hasfound(256,-1);
+        int ans = 0; int start = -1;
+        int L = s.size();
+        for(int i=0; i<L; i++){
+            if(hasfound[s[i]]>start){
+                //elements after "start" can be part of the substring without repeating char
+                start=hasfound[s[i]];
+            }
+            hasfound[s[i]]=i;
+            ans=max(ans,i-start);
+        }
+        return ans;
+    }
+
+//(0128)search longest Consecutive Sequence 
+   int longestConsecutive(vector<int>& nums) {
+        unordered_set<int>bin(nums.begin(), nums.end());
+        int n = nums.size();
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            int span=0;
+            int t = nums[i];
+            while(bin.find(t)!=bin.end()){
+                span++; bin.erase(t);
+                if(t==INT_MAX)break;
+                else t++;
+            }
+            t = nums[i]-1;
+            while(bin.find(t)!=bin.end()){
+                span++; bin.erase(t);
+                if(t==INT_MIN)break;
+                else t--;
+            }
+            ans=max(ans,span);
+        }
+        return ans;
+    }
+//(0274)search the H index (not calculate)
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        vector<int>bin(n,0);
+        int cnt=0;
+        for(int i=0; i<n; i++){
+            if(citations[i]>=n)cnt++;
+            else bin[citations[i]]++;
+        }
+        int h = n;
+        while(cnt<h){
+            cnt+=bin[h-1];h--;
+        }
+        return h;
+    }
+
 /*******************************************************************************************************/
 //(0030) Substring with Concatenation of All Words 
    vector<int> findSubstring(string S, vector<string> &L) {
@@ -138,126 +214,52 @@
     }  
 /*******************************************************************************************************/
 //(0149) max points on a line 
-    unsigned gcd2(unsigned u, unsigned v) {
-        while ( v != 0) {
-            unsigned r = u % v;
-            u = v;
-            v = r;
+    int gcd2(int a, int b){
+        while(b>0){
+            int c = a%b;
+            a=b;
+            b=c;
         }
-        return u;
-    }
-    long getGrad(vector<int>&p1, vector<int>&p2){
-        long grad=0;
-        if(p1[0]-p2[0]==0){
-            grad=((long)(1))<<32;
-        }else if(p1[1]-p2[1]==0){
-            grad=1;
-        }else{
-            long c = (long)gcd2(abs(p1[0]-p2[0]), abs(p1[1]-p2[1]));
-            grad = (((long)(abs(p1[0]-p2[0])/c))&0xffffffff)<<32;
-            grad |= ((long)(abs(p1[1]-p2[1])/c))&0xffffffff;
-            if(p1[0]>p2[0]^p1[1]>p2[1])grad |= (long(1)<<63);
-        }
-        return grad;
+        return a;
     }
     int maxPoints(vector<vector<int>>& points) {
         int n = points.size();
-        if(n<3)return n;
-        int ans=0; 
-        for(int i=0; i<n-1; i++){
-            unordered_map<long, int>grads;
-            int samepoints=0;
-            int pn = 1;
-            for(int j=i+1; j<n; j++){
-                if(points[i][0]==points[j][0]&&points[i][1]==points[j][1]){
-                    samepoints++; continue;
-                }
-                long grad = getGrad(points[i],points[j]);
-                if(grads.find(grad)!=grads.end())grads[grad]+=1;
-                else grads[grad]=2;
-                
-                if(pn<grads[grad])pn=grads[grad];
-            }
-            ans=max(ans,pn+samepoints);
-        }
-        return ans;
-    }
-
-/*******************************************************************************************************/
-// (0209) shortest subarray with sum larger than a number 
-    int minSubArrayLen(int s, vector<int>& nums) {
-        int L = nums.size();
-        if(L<1)return 0;
-        int ans = L+1;
-        int p=0; int q=0; int sum=0;
-        while(q<L){
-            sum+=nums[q];
-            while(sum-nums[p]>=s){
-                sum-=nums[p++];
-            }
-            if(sum>=s&&q-p+1<ans)ans=q-p+1;
-            q++;
-        }
-        return ((ans>L) ? 0 : ans);
-    }
-
-/*******************************************************************************************************/
-//(0003)Longest Substring Without Repeating Characters
-    int lengthOfLongestSubstring(string s) {
-        vector<int>hasfound(256,-1);
-        int ans = 0; int start = -1;
-        int L = s.size();
-        for(int i=0; i<L; i++){
-            if(hasfound[s[i]]>start){
-                //elements after "start" can be part of the substring without repeating char
-                start=hasfound[s[i]];
-            }
-            hasfound[s[i]]=i;
-            ans=max(ans,i-start);
-        }
-        return ans;
-    }
-
-//(0128)search longest Consecutive Sequence 
-   int longestConsecutive(vector<int>& nums) {
-        unordered_set<int>bin(nums.begin(), nums.end());
-        int n = nums.size();
         int ans = 0;
         for(int i=0; i<n; i++){
-            int span=0;
-            int t = nums[i];
-            while(bin.find(t)!=bin.end()){
-                span++; bin.erase(t);
-                if(t==INT_MAX)break;
-                else t++;
+            int samepoint=1;
+            vector<int>p1 = points[i];
+            unordered_map<string,int>lines;
+            for(int j=i+1; j<n; j++){
+                vector<int>p2 = points[j];
+                int dx = p1[0]-p2[0];
+                int dy = p1[1]-p2[1];
+                if(dx==0 && dy==0){
+                    samepoint++;break;
+                }
+                string grad;
+                if(dy==0)grad="0";
+                else if(dx==0)grad="inf";
+                else {
+                    string sign;
+                    if(dy<0 ^ dx<0)sign+='-';
+                    dy=abs(dy); dx=abs(dx);
+                    int t = gcd2(dy,dx);
+                    grad=sign+to_string(dy/t)+to_string(dx/t);
+                }
+                lines[grad]+=1;
             }
-            t = nums[i]-1;
-            while(bin.find(t)!=bin.end()){
-                span++; bin.erase(t);
-                if(t==INT_MIN)break;
-                else t--;
+            int cnt=0;
+            for(auto x:lines){
+                if(x.second>cnt)cnt=x.second;
             }
-            ans=max(ans,span);
+            cnt+=samepoint;
+            if(ans<cnt)ans=cnt;
         }
         return ans;
     }
-//(0274)search the H index (not calculate)
-    int hIndex(vector<int>& citations) {
-        int n = citations.size();
-        vector<int>bin(n,0);
-        int cnt=0;
-        for(int i=0; i<n; i++){
-            if(citations[i]>=n)cnt++;
-            else bin[citations[i]]++;
-        }
-        int h = n;
-        while(cnt<h){
-            cnt+=bin[h-1];h--;
-        }
-        return h;
-    }
 
 /*******************************************************************************************************/
+
 // (0300) LIS
     int lengthOfLIS(vector<int>& nums) {
         set<int>bin;
@@ -287,6 +289,7 @@
         return level.size();
     }
 /*******************************************************************************************************/
+
 //(0239) sliding window maximum
 /*
  * we can easily find the Max of the first window
@@ -300,7 +303,7 @@
         for(int i=0; i<n; i++){
             while(!bin.empty() && nums[bin.back()] < nums[i])bin.pop_back();
             bin.push_back(i);
-            while(!bin.empty() && i-bin.front()>=k)bin.pop_front();
+            if(!bin.empty() && i-bin.front()>=k)bin.pop_front();
             if(i>=k-1)ans.push_back(nums[bin.front()]);
         }
         return ans;
