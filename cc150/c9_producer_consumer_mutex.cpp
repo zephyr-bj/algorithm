d733e7654e58d554a797839273bd6939f5f564d1
zephyr-bj <zephyrutd@gmail.com>
2025-08-03 17:17:56 -0700

    Update c9_producer_consumer_mutex.cpp

--- cc150/c9_producer_consumer_mutex.cpp
+++ cc150/c9_producer_consumer_mutex.cpp
@@ -2,62 +2,60 @@
 
 # include <stdio.h>
 # include <stdlib.h>
-# include <pthread.h>
+# include <thread>
+# include <mutex>
+# include <condition_variable>
 # define BufferSize 10
-
-void *Producer();
-void *Consumer();
-
-int BufferIndex=0;
+static const unsigned int n = 25;
+int head = 0, tail = 0;
 char *BUFFER;
 
-pthread_cond_t Buffer_Not_Full=PTHREAD_COND_INITIALIZER;
-pthread_cond_t Buffer_Not_Empty=PTHREAD_COND_INITIALIZER;
-pthread_mutex_t mVar=PTHREAD_MUTEX_INITIALIZER;
-
+void Producer(int n);
+void Consumer(int n);
+std::mutex mtx;
+std::condition_variable cv;
 int main()
-{    
-    pthread_t ptid,ctid;
-    
-    BUFFER=(char *) malloc(sizeof(char) * BufferSize);            
-    
-    pthread_create(&ptid,NULL,Producer,NULL);
-    pthread_create(&ctid,NULL,Consumer,NULL);
-    
-    pthread_join(ptid,NULL);
-    pthread_join(ctid,NULL);
+{
+    BUFFER=(char *) malloc(sizeof(char) * BufferSize);
+
+    std::thread thd_cons(Consumer, n);
+    std::thread thd_prod(Producer, n);
+
+    thd_prod.join();
+    thd_cons.join();
+
+    free(BUFFER);
 
     return 0;
 }
-
-void * __cdecl Producer(void *)
-{    
-    for(;;)
+void Producer(int n)
+{
+    for(int i = 0; i < n; i++)
     {
-        pthread_mutex_lock(&mVar);
-        if(BufferIndex==BufferSize)
-        {                        
-            pthread_cond_wait(&Buffer_Not_Full,&mVar);
-        }                        
-        BUFFER[BufferIndex++]='@';
-        printf("Produce : %d \n",BufferIndex);
-        pthread_mutex_unlock(&mVar);
-        pthread_cond_signal(&Buffer_Not_Empty);        
-    }    
-    
+        {
+            std::unique_lock<std::mutex> lock(mtx);
+            cv.wait(lock, [](){return ((head + 1) % BufferSize) != tail;});
+            BUFFER[head] = rand() % 256;
+            printf("Produce : %d at %d\n", BUFFER[head], head);
+            head = (head + 1) % BufferSize;
+        }
+        sleep(1);
+        cv.notify_one();
+    }
+
 }
 
-void * __cdecl Consumer(void *)
+void Consumer(int n)
 {
-    for(;;)
+    for(int i = 0; i < n; i++)
     {
-        pthread_mutex_lock(&mVar);
-        if(BufferIndex==-1)
-        {            
-            pthread_cond_wait(&Buffer_Not_Empty,&mVar);
-        }                
-        printf("Consume : %d \n",BufferIndex--);        
-        pthread_mutex_unlock(&mVar);        
-        pthread_cond_signal(&Buffer_Not_Full);                
-    }    
+        {
+            std::unique_lock<std::mutex> lock(mtx);
+            cv.wait(lock, [](){return head != tail;});
+            printf("Consume : %d at %d \n", BUFFER[tail], tail);
+            BUFFER[tail] = '$';
+            tail = (tail + 1) % BufferSize;
+        }
+        cv.notify_one();
+    }
 }
