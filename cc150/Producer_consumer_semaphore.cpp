5b0a0851ec5a9b5ad31acbe70eee75026b1a5f23
zephyr-bj <zephyrutd@gmail.com>
2025-08-03 17:16:56 -0700

    Update Producer_consumer_semaphore.cpp

--- cc150/Producer_consumer_semaphore.cpp
+++ cc150/Producer_consumer_semaphore.cpp
@@ -1,63 +1,57 @@
-# include <Windows.h>
-# include <stdio.h>
-# include <time.h>
+#include <stdio.h>
+#include <time.h>
+#include <semaphore>
+#include <thread>
 
-HANDLE stuff=CreateSemaphore( NULL ,0 ,5 ,NULL);
-HANDLE space=CreateSemaphore( NULL ,5 ,5 ,NULL);
-int buf[5];
-int top = 0, down = 0;
+static const int sz = 5;
+static const int n = 12;
+std::counting_semaphore<sz> sem_stuff(0);
+std::counting_semaphore<sz> sem_space(sz);
+int buf[sz];
+int head = 0, tail = 0;
 
+void producer(int n);
+void consumer(int n);
 int main(void)
 {
-	
-    int j, *tid_arg;
-	srand((int)time(0)); 
-	printf("start\n");
-    tid_arg = (int *) malloc(sizeof(int));
-    *tid_arg = 1;
-	DWORD tid;
-	HANDLE Thread[2];
-	Thread[0]=CreateThread(NULL, 0,(LPTHREAD_START_ROUTINE)producer, (void*)tid_arg, 0, &tid);
-	Thread[1]=CreateThread(NULL, 0,(LPTHREAD_START_ROUTINE)consumer, (void*)tid_arg, 0, &tid);
+    srand((int)time(0));
+    printf("start\n");
+    std::thread thd_producer(producer, n);
+    std::thread thd_consumer(consumer, n);
 
-	WaitForMultipleObjects(2, Thread, TRUE, INFINITE);              
-	system("pause");
+    thd_consumer.join();
+    thd_producer.join();
+    printf("done\n");
     return 0;
 }
 
-void * producer(void *arg)
+void producer(int n)
 {
-	printf("p_in\n");
-    int i,tid,in_data;
-    tid = *((int *) arg);
-    free(arg);
-    for (i=0;i<12;i++){
-		WaitForSingleObject(space, INFINITE);   //space decrease
-		Sleep(rand()%5);
-		in_data=rand()%256;
-        printf(">>p(%d) =(%d),buf[%d]\n", i, in_data,top);
-        buf[top] = in_data;
-        top=(top+1)%5;
-        ReleaseSemaphore(stuff, 1, NULL);
-	}
-    return 0;   
+    printf("p_in\n");
+    int i, in_data;
+    for (i = 0; i < n; i++){
+        sem_space.acquire();
+         sleep(rand()%5);
+        in_data = rand()%256;
+        printf(">>p(%d) =(%d),buf[%d]\n", i, in_data, head);
+        buf[head] = in_data;
+        head = (head + 1) % sz;
+        sem_stuff.release();
+    }
 }
 
-
-void * consumer(void *arg)
+void consumer(int n)
 {
-	printf("c_in\n");
-    int i,tid,out_data;
-    tid = *((int *) arg);
-    free(arg);
-    for (i=0;i<12;i++){
-		WaitForSingleObject(stuff, INFINITE);
-		Sleep(rand()%10);
-		out_data=buf[down];		
-        printf(">>c(%d) =(%d),buf[%d]\n", i, out_data,down);
-		down=(down+1)%5;
-		ReleaseSemaphore(space, 1, NULL); //space increase
-	}
-	return 0;  
+    printf("c_in\n");
+    int i,out_data;
+    for (i = 0; i < n; i++){
+        sem_stuff.acquire();
+        sleep(rand()%10);
+        out_data=buf[tail];
+        printf(">>c(%d) =(%d),buf[%d]\n", i, out_data, tail);
+        tail = (tail + 1) % sz;
+        sem_space.release();
+    }
 }
 
+                                                                                                         1,1           Top
